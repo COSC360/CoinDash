5ed9d9eec43698074c56c2754822a2df6c813244
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-24 21:55:50 -0700

    Testing

--- php/sql-queries.php
+++ php/sql-queries.php
@@ -95,7 +95,7 @@ function uploadDashboard($con, $userId, $dashboardObject){
         mysqli_stmt_execute($dashboardStmt); 
 
         $dashboardObject = json_decode($dashboardObject);
-        print_r($dashboardObject[0]);
+        print_r($dashboardObject);
     } catch (Exception $e){
         echo $e;
     }
