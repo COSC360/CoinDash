6f877ddbe268e4353bf79e0fb44fb0140fe94fc9
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-25 10:33:23 -0700

    Client-side validation

--- js/signIn.js
+++ js/signIn.js
@@ -4,7 +4,7 @@ let usernameError = document.getElementById("usernameError");
 let passwordError = document.getElementById("passwordError");
 // var errorFlag = false;
 var passReg = /^[a-zA-Z]\w{8,16}$/;
-function addErrorMessage($id, $msg){
+window.onload = function addErrorMessage($id, $msg){
     var errorMsg = document.createTextNode($msg);
     if($id = $usernameError){
         usernameError.appendChild(errorMsg);
@@ -12,7 +12,7 @@ function addErrorMessage($id, $msg){
         passwordError.appendChild(errorMsg); 
     }
 }
-function validateLoginForm() {
+window.onload = function validateLoginForm() {
     if (usernameError == "" && password =="") {
         usernameError.style.opacity = "1";
         usernameError.style.visibility = "visible";
@@ -41,12 +41,12 @@ function validateLoginForm() {
     }
   }
 
-function UsernameErrorClearFunction(){
+window.onload = function UsernameErrorClearFunction(){
         usernameError.style.opacity = "0";
         usernameError.style.visibility = "hidden";
 }
 
-function PasswordErrorClearFunction(){
+window.onload = function PasswordErrorClearFunction(){
         passwordError.style.opacity = "0";
         passwordError.style.visibility = "hidden";
 }
