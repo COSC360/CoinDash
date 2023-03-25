37fe210235c0a968f6233c06d4eb79bfb13cb9c6
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-24 21:47:58 -0700

    Testing

--- php/uploadDashboard.php
+++ php/uploadDashboard.php
@@ -4,7 +4,7 @@ include "sql-queries.php";
 
 $userId = $_POST["userId"];
 $dashboardJSON = $_POST["dashboardJSON"];
-    
+echo "<script>console.log('".$dashboardJSON."')</script>";
 uploadDashboard($con, $userId, $dashboardJSON);
     
 ?>
