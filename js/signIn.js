ad3443a8155899ea9ab14fc7057d17ad6e521cdc
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-25 09:16:59 -0700

    Client-side validation

--- js/signIn.js
+++ js/signIn.js
@@ -1,38 +1,34 @@
 function validateLoginForm() {
-    let user = document.forms["LoginForm"]["user-email"].value;
-    let pass = document.forms["LoginForm"]["password"].value;
+    let usernameOrEmail = document.forms["LoginForm"]["user-email"].value;
+    let password = document.forms["LoginForm"]["password"].value;
+    if (usernameOrEmail == "" && password =="") {
+        document.getElementById("usernameEmptyError").style.opacity = "1";
+        document.getElementById("usernameEmptyError").style.visibility = "visible";
+        document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+        document.getElementById("passwordEmptyError").style.opacity = "1";
+        document.getElementById("passwordEmptyError").style.visibility = "visible";
+        document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+      return false;
+    }else if(password ==""){
+        document.getElementById("passwordEmptyError").style.opacity = "1";
+        document.getElementById("passwordEmptyError").style.visibility = "visible";
+        document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+        return false;        
+    }
+    else if(usernameOrEmail ==""){
+        document.getElementById("usernameEmptyError").style.opacity = "1";
+        document.getElementById("usernameEmptyError").style.visibility = "visible";
+        document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+        return false;        
+    }
+  }
 
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        validateInputs();
-    });    
-    
-    const validateInputs = () => {
-        const userOremail = user.value.trim();
-        const password = pass.value.trim();
-    
-        if (userOremail == "" && password =="") {
-            document.getElementById("usernameEmptyError").style.opacity = "1";
-            document.getElementById("usernameEmptyError").style.visibility = "visible";
-            document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-            document.getElementById("passwordEmptyError").style.opacity = "1";
-            document.getElementById("passwordEmptyError").style.visibility = "visible";
-            document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-          return false;
-        }else if(password ==""){
-            document.getElementById("passwordEmptyError").style.opacity = "1";
-            document.getElementById("passwordEmptyError").style.visibility = "visible";
-            document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-            return false;        
-        }
-        else if(userOremail ==""){
-            document.getElementById("usernameEmptyError").style.opacity = "1";
-            document.getElementById("usernameEmptyError").style.visibility = "visible";
-            document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-            return false;        
-        }
-      }
+function UsernameErrorClearFunction(){
+    document.getElementById("usernameEmptyError").style.opacity = "0";
+    document.getElementById("usernameEmptyError").style.visibility = "hidden";
+}
 
-    
-    };
-    
+function PasswordErrorClearFunction(){
+    document.getElementById("usernameEmptyError").style.opacity = "0";
+    document.getElementById("passwordEmptyError").style.visibility = "hidden";
+}
