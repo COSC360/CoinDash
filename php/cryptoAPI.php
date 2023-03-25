b534f5c7475f625e41fb72144dfcac16dbe526ce
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 23:33:08 -0700

    Building API/Test

--- php/cryptoAPI.php
+++ php/cryptoAPI.php
@@ -41,7 +41,7 @@ if ($con->connect_error) {
                 }else{
                     $json = json_decode($response);
                     foreach($json as $item) {
-                        echo $item['image'];
+                        echo $item['market_data']['current_price']['usd'];
                     }
                     // foreach($json as $item) {
                     //     $img_url = $item['image']['large'];
