0137581a7e134b48d95e310eb80f08431f211e1e
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 00:52:36 -0700

    Fixed Number Format

--- php/utils.php
+++ php/utils.php
@@ -1,7 +1,9 @@
 <?php
 
 function formatNumber($num){
+    echo "<script>console.log('".$num."');</script>";
     $str = strval($num);
+    echo "<script>console.log('".$str."');</script>";
     return substr($str, 6);
 }
 
