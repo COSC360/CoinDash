4d0c59ac825b71b94275f81536c567bc3eef2002
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-24 21:34:08 -0700

    Testing

--- php/modules.php
+++ php/modules.php
@@ -1,7 +1,8 @@
 <?php
+echo "<script>console.log('hello4')</script>";
 include "DBconnection.php";
 include "sql-queries.php";
-
+echo "<script>console.log('hello3')</script>";
 if (mysqli_connect_errno()){
     die(mysqli_connect_errno());
 }
