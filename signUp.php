a9c258010162d015ab2afccad86008aef1630d99
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-25 11:56:25 -0700

    Fixin image retrieval

--- signUp.php
+++ signUp.php
@@ -39,14 +39,17 @@ include 'DBconnection.php';
 
                 if($email == "" || $username == "" || $password == "" || $verifyPassword == ""){
                     $statusMsg = 'Please enter all the required details !';
+                    echo "<script>window.alert(".$statusMsg .")</script>"
                 }elseif($password != $verifyPassword){
                     $statusMsg = 'Passwords do not match !';
+                    echo "<script>window.alert(".$statusMsg .")</script>"
                 }elseif($result != null){
                     $statusMsg = 'User already exists !';
+                    echo "<script>window.alert(".$statusMsg .")</script>"
                 }else{
                     // Insert image content into database   
                     $stmt = $con->prepare("INSERT INTO `user_auth` (`Username`, `Email`, `Password`,`comingFrom`,`profilePicture`,`userType`) VALUES (?,?,?,?,?,?)");
-                    $stmt->bind_param("ssssss",$username,$email,$password,$selectedOption,$image,$userType); 
+                    $stmt->bind_param("ssssbs",$username,$email,$password,$selectedOption,$image,$userType); 
                     $stmt->execute();
                     header('location:signIn.php');
                     $stmt->close();
