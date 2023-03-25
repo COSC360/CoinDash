5a396698c180f12961726a23178547ed9376cc6a
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-24 21:49:54 -0700

    Testing

--- php/uploadDashboard.php
+++ php/uploadDashboard.php
@@ -3,9 +3,9 @@ echo "<script>console.log('Hello')</script>";
 include "DBconnection.php";
 include "sql-queries.php";
 
-// echo "<script>console.log('Hello')</script>";
-// $userId = $_POST["userId"];
-// $dashboardJSON = $_POST["dashboardJSON"];
+echo "<script>console.log('Hello')</script>";
+$userId = $_POST["userId"];
+$dashboardJSON = $_POST["dashboardJSON"];
 // echo "<script>console.log('".$dashboardJSON."')</script>";
 // uploadDashboard($con, $userId, $dashboardJSON);
     
