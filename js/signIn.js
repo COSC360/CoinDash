8087910ed4fdc0aa508a9b585a6d31091617fc4c
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-25 09:03:10 -0700

    Building API/Test

--- js/signIn.js
+++ js/signIn.js
@@ -11,26 +11,26 @@ function validateLoginForm() {
         const userOremail = user.value.trim();
         const password = pass.value.trim();
     
-        if (userOremail == "" && password =="") {
-            document.getElementById("usernameEmptyError").style.opacity = "1";
-            document.getElementById("usernameEmptyError").style.visibility = "visible";
-            document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-            document.getElementById("passwordEmptyError").style.opacity = "1";
-            document.getElementById("passwordEmptyError").style.visibility = "visible";
-            document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-          return false;
-        }else if(password ==""){
-            document.getElementById("passwordEmptyError").style.opacity = "1";
-            document.getElementById("passwordEmptyError").style.visibility = "visible";
-            document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-            return false;        
-        }
-        else if(userOremail ==""){
-            document.getElementById("usernameEmptyError").style.opacity = "1";
-            document.getElementById("usernameEmptyError").style.visibility = "visible";
-            document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
-            return false;        
-        }
+        // if (userOremail == "" && password =="") {
+        //     document.getElementById("usernameEmptyError").style.opacity = "1";
+        //     document.getElementById("usernameEmptyError").style.visibility = "visible";
+        //     document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+        //     document.getElementById("passwordEmptyError").style.opacity = "1";
+        //     document.getElementById("passwordEmptyError").style.visibility = "visible";
+        //     document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+        //   return false;
+        // }else if(password ==""){
+        //     document.getElementById("passwordEmptyError").style.opacity = "1";
+        //     document.getElementById("passwordEmptyError").style.visibility = "visible";
+        //     document.getElementById("passwordEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+        //     return false;        
+        // }
+        // else if(userOremail ==""){
+        //     document.getElementById("usernameEmptyError").style.opacity = "1";
+        //     document.getElementById("usernameEmptyError").style.visibility = "visible";
+        //     document.getElementById("usernameEmptyError").style.transition = "visibility 0s linear 0s, opacity 300ms";
+        //     return false;        
+        // }
       }
 
     
