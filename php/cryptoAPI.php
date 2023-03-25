d6dfcd0f756d925c766fb3b0c0d440e989b406c0
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-24 23:00:58 -0700

    Building API/Test

--- php/cryptoAPI.php
+++ php/cryptoAPI.php
@@ -5,8 +5,9 @@ include "DBconnection.php";
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }else{
-    
-    $stmt = $con->prepare("SELECT * FROM coin");
+    for($i=0; $i < 100; $i + 10){
+    $stmt = $con->prepare("SELECT * FROM coin LIMIT 10 OFFSET ?");
+    $stmt->bind_param("i", $i);
     $stmt->execute();
     $resultSet = $stmt->get_result(); // get the mysqli result
     $result = $resultSet->fetch_all(MYSQLI_ASSOC);
@@ -29,17 +30,26 @@ if ($con->connect_error) {
             ],
         ]);
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            $json = json_decode($response, true);
-            echo "<script>console.log(".$json.");</script>";
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                if ($con->connect_error) {
+                    die("Connection failed: " . $con->connect_error);
+                }else{
+                    $json = json_decode($response);
+                    print_r($json);
+                    // foreach($json['image'] as $item) {
+                    //     $img_url = $item['large'];
+                    // $insertStmt = $con->prepare("INSERT INTO `coin` (`img_url`, `usd`,`cad`,`eur`,`php`,`jpy`,`price_change_24h`,`price_change_7d`,`price_change_14d`,`price_change_30d`,`price_change_60d`,`price_change_200d`,`price_change_1yr`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?) WHERE `Id` = ?");
+                    // $insertStmt->bind_param("sdddddddddddds", $img_url,$coinCategory,$field['Id']); 
+                    // $insertStmt->execute();
+                }
+            }
         }
-
     }
 }
 
