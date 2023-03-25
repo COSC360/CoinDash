0025ff8ecf31b435a7ba2fe254a0d6a3b010955a
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 16:43:47 -0700

    Testing Modulization

--- php/header.php
+++ php/header.php
@@ -1,38 +1,29 @@
-<?php
-
-function generateHeader(){
-
-    return "
-        <header class=\"container\">
-            <div class=\"fill-container\">
-                <div class=\"left\">
-                    <p class=\"logo\">
-                        LOGO HERE
-                    </p>
-                </div>
-                <div class=\"middle\">
-                    <nav>
-                        <a href=\"#\">Search</a>
-                        <a href=\"#\">My Dashboard</a>
-                        <a href=\"#\">Community</a>
-                        <a href=\"#\">Help</a>
-                    </nav>
-                </div>
-                <div class=\"right settings\">
-                    <div class=\"horizontal-container fit-width\">
-                        <p>English-US</p>
-                        <img src=\"images/canada-flag.png\">
-                        <img src=\"svgs/arrow-down.svg\">
-                    </div>
-                    <div class=\"horizontal-container fit-width\">
-                        <p>Hi, Jason</p>
-                        <img src=\"images/profile-picture.png\">
-                        <img src=\"svgs/arrow-down.svg\">
-                    </div>
-                </div>
+<header class="container">
+    <div class="fill-container">
+        <div class="left">
+            <p class="logo">
+                LOGO HERE
+            </p>
+        </div>
+        <div class="middle">
+            <nav>
+                <a href="#">Search</a>
+                <a href="#">My Dashboard</a>
+                <a href="#">Community</a>
+                <a href="#">Help</a>
+            </nav>
+        </div>
+        <div class="right settings">
+            <div class="horizontal-container fit-width">
+                <p>English-US</p>
+                <img src="images/canada-flag.png">
+                <img src="svgs/arrow-down.svg">
             </div>
-        </header>
-    ";
-}
-
-?>
+            <div class="horizontal-container fit-width">
+                <p>Hi, Jason</p>
+                <img src="images/profile-picture.png">
+                <img src="svgs/arrow-down.svg">
+            </div>
+        </div>
+    </div>
+</header>
