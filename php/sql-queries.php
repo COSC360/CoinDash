c81a203e92ed0f8495f6913646f5ddae45c9d0e8
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 21:57:57 -0700

    Initial Dashboard Select Listeners

--- php/sql-queries.php
+++ php/sql-queries.php
@@ -27,9 +27,8 @@ function retrieveAllCoins($con){
 }
 
 function retrieveCoinsByCategory($con, $fiat, $category, $sort, $perPage, $page){
-    echo "<script>console.log('Hi')</script>";
     $sql = "SELECT symbol, img, ?, price_Change_24h, price_change_7d, price_change_14d, price_change_30d, price_change_60d, price_change_200d, price_change_1yr "
-        + "FROM coin WHERE id IN (SELECT coin FROM categoryCoin WHERE category = ?) ORDER BY ? LIMIT ? OFFSET ?";
+        + "FROM coin WHERE id IN (SELECT coin FROM categoryCoin WHERE category = ?) LIMIT ? OFFSET ?";
     $stmt = mysqli_stmt_init($con);
 
     if (!mysqli_stmt_prepare($stmt, $sql)){
@@ -37,15 +36,15 @@ function retrieveCoinsByCategory($con, $fiat, $category, $sort, $perPage, $page)
         // header("location: REPLACE LATER");
         exit();
     }
-    echo "1";
+
     $offset = ($page - 1) * $perPage;
-    echo "1";
+
     // Set parameters for prepared statement
-    mysqli_stmt_bind_param($stmt, "ssssii", $fiat, $category, $sort, $perPage, $offset);
-    echo "1";
+    mysqli_stmt_bind_param($stmt, "ssii", $fiat, $category, $perPage, $offset);
+
     // Execute prepared statement
     mysqli_stmt_execute($stmt);
-    echo "1";
+
     $results = mysqli_stmt_get_result($stmt);
 
     if ($rows = $results -> fetch_all(MYSQL_ASSOC)){
