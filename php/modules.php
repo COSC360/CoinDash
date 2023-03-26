0e56e4d6e4834170bb636672035fcdc070994856
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 11:25:31 -0700

    Dashboard Sort By Dropdown

--- php/modules.php
+++ php/modules.php
@@ -12,7 +12,7 @@ $fiats = array("usd", "cad", "eur", "php", "jpy");
 $sortLabels = array("24H: High To Low", "24H: Low To High", "7D: High To Low", "7D: Low To High", "14D: High To Low", "14D: Low To High", 
                 "30D: High To Low", "30D: Low To High", "60D: High To Low", "60D: Low To High",
                 "200D: High To Low", "200D: Low To High", "1YR: High To Low", "1YR: Low To High");
-$sortValues = array("price_change_24h", "price_change_24hDESC", "price_change_7d", "price_change_7d DESC", "price_change_14d", "price_change_14d DESC",
+$sortValues = array("price_change_24h", "price_change_24h DESC", "price_change_7d", "price_change_7d DESC", "price_change_14d", "price_change_14d DESC",
                 "price_change_30d", "price_change_30d DESC", "price_change_60d", "price_change_60d DESC",
                 "price_change_200d", "price_change_200d DESC", "price_change_1yr", "price_change_1yr DESC");
 
