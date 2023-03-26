79c1d45575f59b8b0fac56eac32dd72583977334
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 16:30:37 -0700

    User Image Fix

--- php/pfpHandler.php
+++ php/pfpHandler.php
@@ -10,7 +10,7 @@ session_start();
         const reader = new FileReader();
 
         reader.addEventListener("load", ()=>{
-            localStorage.setItem(<?php echo $_SESSION['Id']?>, reader.result);
+            localStorage.setItem("<?php echo $_SESSION['Id']?>", reader.result);
         })
 
         reader.readAsDataURL(this.files[0])
