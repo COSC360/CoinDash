04b6f1fd95cef87b7d50e2b810732ad1f17b7333
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 20:00:32 -0700

    Initial Dashboard Select Listeners

--- php/dashboard.php
+++ php/dashboard.php
@@ -60,7 +60,7 @@
                                         </a>    
                                     </div>
                                     <div class=\"dropdowns\">
-                                        <select class=\"dropdown fiat\" onchange=changeFiat('Hellooasdfasfjnia')>";
+                                        <select class=\"dropdown fiat\" onchange=changeFiat('{asfkj nashhgbjfk}')>";
                                             for ($i = 0; $i < sizeof($fiats); $i++){
                                                 echo "<option value=".$fiats[$i]." ".($fiats[$i] == $dashboardModules[$currentCount]["fiat"] ? 'selected' : '').">".$fiatLabels[$i]."</option>";
                                             }
