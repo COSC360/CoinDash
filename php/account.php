243bb033eb0cab080a46268c25ac6e77ba1b2ce4
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 16:17:52 -0700

    User Image Fix

--- php/account.php
+++ php/account.php
@@ -46,7 +46,7 @@ if($_SESSION['Id'] == null){
             <div class="user-account-box">
                 <div class="profile-box">
                     <!-- <div id="centered">Upload<br>Photo</div> -->
-                    <?php include 'pfpHandler.php';?>
+    
                     <img src="" id = "pfp"> 
                     <h1>Username</p>
                     <h2><?php echo $_SESSION["user"] ;?></h2>
--- php/pfpHandler.php
+++ php/pfpHandler.php
@@ -10,18 +10,11 @@ session_start();
         const reader = new FileReader();
 
         reader.addEventListener("load", ()=>{
-            localStorage.setItem(<?php echo $_SESSION['Id']?>, reader.result);
+            console.log(reader.result);
         })
 
         reader.readAsDataURL(this.files[0])
     });
 
-    document.addEventListener("DOMContentLoaded", () =>{
-        const pfpDataURL = localStorage.getItem(<?php echo $_SESSION['Id']?>);
-
-        if(pfpDataURL){
-            document.querySelector("pfp").setAttribute("src",pfpDataURL);
-        }
-    });
 </script>
 
