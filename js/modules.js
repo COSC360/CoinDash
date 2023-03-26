03178e4164b3f827765b1031f827795f3c0a28b1
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 12:40:10 -0700

    Initial Module Settings Modal

--- js/modules.js
+++ js/modules.js
@@ -17,7 +17,7 @@ module_settings_btns.forEach(btn => {
     btn.addEventListener("click", (e) => {
         btn.parentNode.appendChild(moduleModal);
         // var btnRect = btn.getBoundingClientRect();
-        // moduleModal.classList.remove("hide");
+        moduleModal.classList.remove("hide");
         // moduleModal.style.left = (btnRect.left + 20) + "px";
         // moduleModal.style.top = btnRect.top + "px";
     })
