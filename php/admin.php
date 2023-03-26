8f6c608fe6e5112f4bfe4d20907e0651e74df09f
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 08:10:58 -0700

    Admin Functionality v0.1

--- php/admin.php
+++ php/admin.php
@@ -1,13 +1,13 @@
 <?php
-    session_start();
+    // session_start();
 
-    if($_SESSION["userType"] == "user"){
-        header('location:adminAuth.php');
-    }elseif($_SESSION["userType"] == null){
-        header('location:adminAuth.php');
-    }else{
-        echo "ou've landed on the admin page !"
-    }
+    // if($_SESSION["userType"] == "user"){
+    //     header('location:adminAuth.php');
+    // }elseif($_SESSION["userType"] == null){
+    //     header('location:adminAuth.php');
+    // }else{
+    //     echo "ou've landed on the admin page !"
+    // }
     //Add logic based on user type
     
 ?>
