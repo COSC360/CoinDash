f23c1bd742b846394ca3aaed70118ef4a0615abf
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 19:53:33 -0700

    Initial Dashboard Select Listeners

--- php/dashboard.php
+++ php/dashboard.php
@@ -14,6 +14,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="../js/jquery-3.1.1.min.js"></script>
     <script src="../js/dashboard.js"></script>
+    <script src="../js/moduleListeners.js"></script>
 </head>
 
 <body>
