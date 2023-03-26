b7e69222580cb5f5527e6dcf65b8f5333a5c77ea
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 21:53:50 -0700

    Initial Dashboard Select Listeners

--- php/sql-queries.php
+++ php/sql-queries.php
@@ -27,9 +27,7 @@ function retrieveAllCoins($con){
 }
 
 function retrieveCoinsByCategory($con, $fiat, $category, $sort, $perPage, $page){
-    error_reporting(E_ALL);
-
-    in_set('display_errors', '1');
+    echo "1";
     $sql = "SELECT symbol, img, ?, price_Change_24h, price_change_7d, price_change_14d, price_change_30d, price_change_60d, price_change_200d, price_change_1yr "
         + "FROM coin WHERE id IN (SELECT coin FROM categoryCoin WHERE category = ?) ORDER BY ? LIMIT ? OFFSET ?";
     $stmt = mysqli_stmt_init($con);
@@ -39,15 +37,15 @@ function retrieveCoinsByCategory($con, $fiat, $category, $sort, $perPage, $page)
         // header("location: REPLACE LATER");
         exit();
     }
-
+    echo "1";
     $offset = ($page - 1) * $perPage;
-
+    echo "1";
     // Set parameters for prepared statement
     mysqli_stmt_bind_param($stmt, "ssssii", $fiat, $category, $sort, $perPage, $offset);
- 
+    echo "1";
     // Execute prepared statement
     mysqli_stmt_execute($stmt);
-
+    echo "1";
     $results = mysqli_stmt_get_result($stmt);
 
     if ($rows = $results -> fetch_all(MYSQL_ASSOC)){
