707151273ce0ca5955a37bb407545b82a62d8229
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 21:10:06 -0700

    Initial Dashboard Select Listeners

--- js/moduleListeners.js
+++ js/moduleListeners.js
@@ -7,6 +7,8 @@ fiatSelects.forEach(fiatSelect => {
     fiatSelect.addEventListener('change', (e) => {
         var newFiat = fiatSelect.value;
         var moduleId = fiatSelect.parentNode.parentNode.id;
+        console.log(fiatSelect.parentNode);
+        console.log(fiatSelect.parentNode.parentNode);
         console.log(moduleId)
         var module = buildModule(moduleId);
 
