62f019196aebe42d17e67beb2875cab8820331eb
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 08:40:35 -0700

    client-side validation

--- js/signUp.js
+++ js/signUp.js
@@ -1,6 +1,6 @@
 
 
-window.onload = function validateLoginForm() {
+window.onload = function validateRegisterForm() {
     let username = document.getElementById("username");
     let password = document.getElementById("password");
     let email = document.getElementById("email");
@@ -83,9 +83,9 @@ window.onload = function validateLoginForm() {
     }
 
     if(!passReg.test(password)){
-        // passwordError.style.opacity = "1";
-        // passwordError.style.visibility = "visible";
-        // passwordError.style.transition = "visibility 0s linear 0s, opacity 300ms";
+        passwordError.style.opacity = "1";
+        passwordError.style.visibility = "visible";
+        passwordError.style.transition = "visibility 0s linear 0s, opacity 300ms";
         return false;
     }
 
