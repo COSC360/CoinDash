536be8344ee769063b95b3d2ea1719bd2f4cb779
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 21:10:48 -0700

    Initial Dashboard Select Listeners

--- js/moduleListeners.js
+++ js/moduleListeners.js
@@ -6,7 +6,7 @@ var sortSelects = document.querySelectorAll(".sort");
 fiatSelects.forEach(fiatSelect => {
     fiatSelect.addEventListener('change', (e) => {
         var newFiat = fiatSelect.value;
-        var moduleId = fiatSelect.parentNode.parentNode.id;
+        var moduleId = fiatSelect.parentNode.parentNode.parentNode.id;
         console.log(fiatSelect.parentNode);
         console.log(fiatSelect.parentNode.parentNode);
         console.log(moduleId)
