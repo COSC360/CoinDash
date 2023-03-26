1673c0d2d62fa8dc86f0cc9e26ac171d8904d8d6
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 11:11:36 -0700

    Initial Dashboard Select Listeners

--- php/sql-queries.php
+++ php/sql-queries.php
@@ -27,9 +27,16 @@ function retrieveAllCoins($con){
 }
 
 function retrieveCoinsByCategory($con, $fiat, $category, $sort, $perPage, $page){
-    
-    $sql = "SELECT symbol, name, img_url, ".$fiat.", price_change_24h, price_change_7d, price_change_14d, price_change_30d, price_change_60d, price_change_200d, price_change_1yr FROM coin 
-           WHERE id IN (SELECT coin FROM coinCategory WHERE category = ?) ORDER BY ? LIMIT ? OFFSET ?;";
+    $validSortValues = array("price_change_24h", "price_change_24h DESC", "price_change_7d", "price_change_7d DESC", "price_change_14d", "price_change_14d DESC",
+                "price_change_30d", "price_change_30d DESC", "price_change_60d", "price_change_60d DESC",
+                "price_change_200d", "price_change_200d DESC", "price_change_1yr", "price_change_1yr DESC");
+
+    if (!in_array($sort, $validSortValues)){
+        // TODO:
+        // header("location: REPLACE LATER");
+        exit();
+    }
+    $sql = "SELECT * FROM coin WHERE id IN (SELECT coin FROM coinCategory WHERE category = ?) ORDER BY ".$sort." LIMIT ? OFFSET ?;";
     //ORDER BY ? LIMIT ? OFFSET ?;
 
     //WHERE id IN (SELECT coin_id FROM categoryCoin WHERE category = ?) LIMIT ? OFFSET ?
