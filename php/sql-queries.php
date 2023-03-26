d392933896bf6b8de7fb086a2b79e1bb3a61e5da
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-25 22:26:02 -0700

    Initial Dashboard Select Listeners

--- php/sql-queries.php
+++ php/sql-queries.php
@@ -28,7 +28,7 @@ function retrieveAllCoins($con){
 
 function retrieveCoinsByCategory($con, $fiat, $category, $sort, $perPage, $page){
     
-    $sql = "SELECT symbol, name, img_url, ".$fiat.", price_change_24H, price_change_7D, price_change_14D, price_change_30D, price_change_60D, price_change_200D, price_change_1yr FROM coin 
+    $sql = "SELECT symbol, name, img_url, ".$fiat.", price_change_24h, price_change_7d, price_change_14d, price_change_30d, price_change_60d, price_change_200d, price_change_1yr FROM coin 
            WHERE id IN (SELECT coin FROM coinCategory WHERE category = ?) ORDER BY ? LIMIT ? OFFSET ?;";
     //ORDER BY ? LIMIT ? OFFSET ?;
 
