ae2cc108226f200023c9d7404f587f6db28f3175
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 21:26:41 -0700

    Admin Page Buildup

--- php/admin.php
+++ php/admin.php
@@ -45,7 +45,7 @@
             $selectUserByCommentstmt->execute();
             $resultSetselectUserByCommentstmt = $selectUserByCommentstmt->get_result(); // get the mysqli result
             $resultselectUserByComment = $resultSetselectUserByCommentstmt->fetch_all(MYSQLI_ASSOC);
-
+            $_SESSION['postId'] = $resultselectUserByComment;
         }
 
     }
