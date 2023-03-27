a988cfeab4502b630920de5f459ea9c14e28a454
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 20:41:48 -0700

    Adjusted Nav Spacing

--- js/modules.js
+++ js/modules.js
@@ -8,51 +8,6 @@ var editElements = document.querySelectorAll(".edit-ui");
 var toggleEditBtn = document.getElementById("view-edit-btn");
 var saveEditBtn = document.getElementById("save-edit-btn");
 var moduleModal = document.getElementById("module-modal");
-var moduleForm = document.getElementById("module-form");
-var closeModalBtn = document.getElementById("modal-close-btn");
-var cancelModalBtn = document.getElementById("modal-cancel-btn");
-var confirmModalBtn = document.getElementById("modal-confirm-btn");
-var currentModule = null;
-
-closeModalBtn.addEventListener("click", (e) => {
-    moduleModal.classList.add("hide");
-})
-
-cancelModalBtn.addEventListener("click", (e) => {
-    moduleModal.classList.add("hide");
-})
-
-module_settings_btns.forEach(btn => {
-    // Grab connectedModule to be moved
-    btn.addEventListener("dragstart", (e) => {
-        e.dataTransfer.setData("draggedModule", btn.parentNode.id); 
-        e.dataTransfer.setDragImage(btn.parentNode, 0, 0);
-    })
-
-    btn.addEventListener("click", (e) => {
-        btn.parentNode.appendChild(moduleModal);
-        moduleModal.classList.remove("hide");
-        currentModule = btn.parentNode;
-    })
-})
-
-confirmModalBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    var categoryElement = document.querySelector("#" + currentModule.id + " .api-category");
-    var fiatElement = document.querySelector("#" + currentModule.id + " .fiat");
-    var sortElement = document.querySelector("#" + currentModule.id + " .sort");
-    var moduleGallery = document.querySelector("#" + currentModule.id + " .module-gallery");
-
-    var fiat = moduleForm.fiat.value;
-    var category = moduleForm.category.value;
-    var sort = moduleForm.sort.value;
-
-    categoryElement.innerHTML = category;
-    fiatElement.value = fiat;
-    sortElement.value = sort;
-    setModuleHTML(fiat, category, sort, moduleGallery);
-    moduleModal.classList.add("hide");
-})
 
 modules.forEach(module => {
     var dragCounter = 0;
@@ -130,3 +85,57 @@ saveEditBtn.addEventListener("click", (e) => {
     e.preventDefault();
     uploadDashboard(1);
 })
+
+attachModalListeners();
+
+function attachModalListeners(){
+    if (moduleModal == null){
+        return;
+    }
+    var moduleForm = document.getElementById("module-form");
+    var closeModalBtn = document.getElementById("modal-close-btn");
+    var cancelModalBtn = document.getElementById("modal-cancel-btn");
+    var confirmModalBtn = document.getElementById("modal-confirm-btn");
+    var currentModule = null;
+    
+    closeModalBtn.addEventListener("click", (e) => {
+        moduleModal.classList.add("hide");
+    })
+    
+    cancelModalBtn.addEventListener("click", (e) => {
+        moduleModal.classList.add("hide");
+    })
+    
+    module_settings_btns.forEach(btn => {
+        // Grab connectedModule to be moved
+        btn.addEventListener("dragstart", (e) => {
+            e.dataTransfer.setData("draggedModule", btn.parentNode.id); 
+            e.dataTransfer.setDragImage(btn.parentNode, 0, 0);
+        })
+    
+        btn.addEventListener("click", (e) => {
+            btn.parentNode.appendChild(moduleModal);
+            moduleModal.classList.remove("hide");
+            currentModule = btn.parentNode;
+        })
+    })
+    
+    confirmModalBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        var categoryElement = document.querySelector("#" + currentModule.id + " .api-category");
+        var fiatElement = document.querySelector("#" + currentModule.id + " .fiat");
+        var sortElement = document.querySelector("#" + currentModule.id + " .sort");
+        var moduleGallery = document.querySelector("#" + currentModule.id + " .module-gallery");
+    
+        var fiat = moduleForm.fiat.value;
+        var category = moduleForm.category.value;
+        var sort = moduleForm.sort.value;
+    
+        categoryElement.innerHTML = category;
+        fiatElement.value = fiat;
+        sortElement.value = sort;
+        setModuleHTML(fiat, category, sort, moduleGallery);
+        moduleModal.classList.add("hide");
+    })
+    
+}
