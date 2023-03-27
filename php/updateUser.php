a9e1ec1ffa70ab159cb724ea6644fb32c9f365c4
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 22:14:21 -0700

    Admin Page Buildup

--- php/updateUser.php
+++ php/updateUser.php
@@ -2,6 +2,7 @@
 session_start();
 
 echo $_SESSION['postId'];
+echo $_POST['commentText'];
     if(isset($_POST['commentText'])){
         $commentText = $_POST['commentText'];
         //Search user by name
