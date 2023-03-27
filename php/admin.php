76fb134508e34f718f7956827a4353258a3e6cb4
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 22:27:30 -0700

    Admin Page Buildup

--- php/admin.php
+++ php/admin.php
@@ -6,6 +6,7 @@
     if ($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
     }else{
+
         if(isset($_POST['searchByName'])){
             $searchByName = $_POST['searchByName'];
             //Search user by name
@@ -36,7 +37,7 @@
             $resultSetselectUserByCommentstmt = $selectUserByCommentstmt->get_result(); // get the mysqli result
             $resultselectUserByComment = $resultSetselectUserByCommentstmt->fetch_all(MYSQLI_ASSOC);
             $_SESSION["postId"] = $resultselectUserByComment[0]['id'];
-            // $_SESSION["commentText"] = $_POST['commentText'];
+            $_SESSION["commentText"] = $resultselectUserByComment[0]['text'];
         }
 
 
@@ -101,10 +102,10 @@
                 <p>User Username : <?php echo $resultselectUserByComment[0]['Username']?></p>
                 <p>User Email : <?php echo $resultselectUserByComment[0]['Email']?></p>
                 <p>User Status : <?php echo $resultselectUserByComment[0]['status']?></p>
-                    <!-- <form action="" method ="POST">
+                    <form action="../php/updateUser.php" method ="POST">
                         <input type="text" name ="commentText" id="commentText" value="<?php echo $resultselectUserByComment[0]['text']?>">
                         <input type="submit" name="submit" value="change">
-                    </form> -->
+                    </form>
                 
             </div>
         </form>
