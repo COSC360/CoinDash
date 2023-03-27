369607baad2a3182d3ec402188ed7cc51d743be5
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 21:02:42 -0700

    Adjusted Nav Spacing

--- js/modules.js
+++ js/modules.js
@@ -1,5 +1,4 @@
 import { uploadDashboard } from "./uploadDashboard.js";
-import { setModuleHTML } from "./moduleListeners.js";
 import { buildModule } from "./parser.js";
 
 var module_settings_btns = document.querySelectorAll(".module-settings-btn");
@@ -133,3 +132,71 @@ saveEditBtn.addEventListener("click", (e) => {
     e.preventDefault();
     uploadDashboard(1);
 })
+
+
+var fiatSelects = document.querySelectorAll(".fiat");
+var sortSelects = document.querySelectorAll(".sort");
+
+// Change product cards by fiat
+fiatSelects.forEach(fiatSelect => {
+    fiatSelect.addEventListener('change', (e) => {
+        var newFiat = fiatSelect.value;
+        var module = fiatSelect.parentNode.parentNode.parentNode;
+        var moduleObj = buildModule(module);
+        var moduleGallery = document.querySelector("#" + module.id + " .module-gallery");
+
+        setModuleHTML(newFiat, moduleObj.category, moduleObj.sort, moduleGallery);
+    })
+});
+
+// Change product cards by sort
+sortSelects.forEach(sortSelect => {
+    sortSelect.addEventListener('change', (e) => {
+        var newSort = sortSelect.value;
+        var module = sortSelect.parentNode.parentNode.parentNode;
+        var moduleObj = buildModule(module);
+        var moduleGallery = document.querySelector("#" + module.id + " .module-gallery");
+
+        setModuleHTML(moduleObj.fiat, moduleObj.category, newSort, moduleGallery);
+    })
+})
+
+function setModuleHTML(fiat, category, sort, target){
+    $.ajax({
+        url: "./retrieveModuleItems.php",
+        type: "POST",
+        async: true,
+        data: {fiat: fiat, category: category, sort: sort},
+        success: function(response) {
+            var coinData = JSON.parse(response);
+            let newModule = "";
+            
+            coinData.forEach((coin) => {
+                newModule += `
+                    <div class="product-card">
+                        <div class="icon-container">
+                            <a class="icon-overlay" onclick=navigateToIndividualPage("${coin.Id}")>
+                                <img src="../svgs/goto.svg">
+                            </a>
+                        </div>
+                            <div class="product-image-mask">
+                            <div class="product-image" style="background-image: url(${coin.img_url});"></div>
+                        </div><div class="product-info-container">
+                            <h3>${coin.name}</h3>
+                            <strong class="product-price">${coin[fiat].toFixed(4)}${fiat.toUpperCase()} ${coin.price_change_24h.toFixed(2)}%</strong>
+                            <div class="price-trend-container">
+                                <p>7D: ${coin.price_change_7d.toFixed(2)}%</p>
+                                <p>14D: ${coin.price_change_14d.toFixed(2)}%</p>
+                                <p>30D: ${coin.price_change_30d.toFixed(2)}%</p>
+                                <p>60D: ${coin.price_change_60d.toFixed(2)}%</p>
+                                <p>200D: ${coin.price_change_200d.toFixed(2)}%</p>
+                                <p>1Y: ${coin.price_change_1yr.toFixed(2)}%</p>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+            })
+            target.innerHTML = newModule;
+        }
+    })
+}
