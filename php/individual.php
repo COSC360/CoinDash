3a386d5f7df62fa65e5b219c45c4ee7facff5338
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 21:42:18 -0700

    Adjusted Nav Spacing

--- php/individual.php
+++ php/individual.php
@@ -4,7 +4,8 @@ session_set_cookie_params(0);
 session_start();
 
 if($_SESSION['Id'] == null){
-    header('location:signIn.php');
+    echo "<script>alert('You must be a registered user to access this page!');</script>";
+    header('location: signIn.php');
 }
 ?>
 
