306512c5bc482012ff7d43ed8bf574bdb8f2c863
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 21:16:19 -0700

    Adjusted Nav Spacing

--- php/edit.php
+++ php/edit.php
@@ -2,13 +2,12 @@
 <?php
     if($_SESSION['Id'] != null){
         echo "
-        <div id=\"view-edit-btn\" class=\"icon-overlay\" href=\"#\">
-            <img src=\"../svgs/view.svg\">
+        <div id=\"save-edit-btn\" class=\"icon-overlay edit-ui\" href=\"#\">
+            <img src=\"../svgs/save.svg\">
         </div>
         ";
     }
 ?>
-
-<div id="save-edit-btn" class="icon-overlay edit-ui" href="#">
-    <img src="../svgs/save.svg">
+<div id="view-edit-btn" class="icon-overlay" href="#">
+    <img src="../svgs/view.svg">
 </div>
