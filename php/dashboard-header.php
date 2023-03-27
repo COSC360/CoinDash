f9670d68968b4e5c36c60eea9df74ef35478b237
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 17:45:11 -0700

    User Image Fix

--- php/dashboard-header.php
+++ php/dashboard-header.php
@@ -27,7 +27,7 @@ session_start();
             <?php
                 if($_SESSION['user'] != null){
                     echo  "<p>Hi, ".$_SESSION['user']."</p>";
-                    echo  "<img src=\"".$_SESSION['pfp']."  style=\"width:40px; height:40px;\">";
+                    echo  "<img src=".$_SESSION['pfp']." style=\"width: 40px;height:40px;\">";
                     echo  "<img src=\"../svgs/arrow-down.svg\">";
                 }else{
                     echo  "Sign In / Sign Up";       
