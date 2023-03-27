3f722bc6cb621d1a144f8390aaa2862d176ca158
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 22:16:04 -0700

    Admin Page Buildup

--- php/updateUser.php
+++ php/updateUser.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-echo $_SESSION['postId'];
-echo $_POST['commentText'];
-    if(isset($_POST['commentText'])){
-        $commentText = $_POST['commentText'];
-        //Search user by name
-        $stmt = $con->prepare("UPDATE comment SET `text` = ? WHERE `id` = ?");
-        $stmt->bind_param("si", $commentText,$_SESSION['postId']); 
-        $stmt->execute();
-    }
+echo $_SESSION['commentText'];
+
+    // if(isset($_POST['commentText'])){
+    //     $commentText = $_POST['commentText'];
+    //     //Search user by name
+    //     $stmt = $con->prepare("UPDATE comment SET `text` = ? WHERE `id` = ?");
+    //     $stmt->bind_param("si", $commentText,$_SESSION['postId']); 
+    //     $stmt->execute();
+    // }
 ?>
