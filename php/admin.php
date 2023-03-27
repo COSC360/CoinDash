91d243e79853de77c2f4ae069ef6a808cd2982ae
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-03-26 18:55:10 -0700

    Admin Page Buildup

--- php/admin.php
+++ php/admin.php
@@ -12,7 +12,7 @@
         $resultSetcmmt = $cmmtstmt->get_result(); // get the mysqli result
         $resultcmmt = $resultSetcmmt->fetch_assoc();
         
-        echo "<script>console.log(".$resultcmmt.")</script>";
+        echo "<script>console.log(".print_r($resultcmmt).")</script>";
         // $_SESSION['comment'] = $resultcmmt['text'];
     }
 
