f8a3b441df84f2ccc4717e7a27a3c35a623f3f11
Jason Ramos <ramosjasonm.work@gmail.com>
2023-03-26 19:33:31 -0700

    Module Navigation

--- php/searchPage.php
+++ php/searchPage.php
@@ -39,32 +39,36 @@ session_start();
         </article>
         <article id="dashboard">
             <?php 
+
                 echo "<div class=\"module-gallery\">";
-                    $coins = retrieveCoinByLike($con, "d");
-                    foreach($coins as $coin){
-                        echo 
-                        "<div class=\"product-card\">
-                            <div class=\"icon-container\">
-                                <a class=\"icon-overlay\" onclick=navigateToIndividualPage(\"".$coin["Id"]."\")>
-                                    <img src=\"../svgs/goto.svg\">
-                                </a>
+
+                include "sql-queries.php";
+                $coins = retrieveCoinByLike($con, "d");
+                print_r($coins);
+                foreach($coins as $coin){
+                    echo 
+                    "<div class=\"product-card\">
+                        <div class=\"icon-container\">
+                            <a class=\"icon-overlay\" onclick=navigateToIndividualPage(\"".$coin["Id"]."\")>
+                                <img src=\"../svgs/goto.svg\">
+                            </a>
+                        </div>
+                            <div class=\"product-image-mask\">
+                            <div class=\"product-image\" style=\"background-image: url(".$coin["img_url"].");\"></div>
+                        </div><div class=\"product-info-container\">
+                            <h3>".$coin["name"]."</h3>
+                            <strong class=\"product-price\">".number_format($coin[$dashboardModules[$currentCount]["fiat"]], 4, '.', '')." ".strtoupper($dashboardModules[$currentCount]["fiat"])." ".number_format($coin["price_change_24h"], 2, '.', '')."%</strong>
+                            <div class=\"price-trend-container\">
+                                <p>7D: ".number_format($coin["price_change_7d"], 2, '.', '')."%</p>
+                                <p>14D: ".number_format($coin["price_change_14d"], 2, '.', '')."%</p>
+                                <p>30D: ".number_format($coin["price_change_30d"], 2, '.', '')."%</p>
+                                <p>60D: ".number_format($coin["price_change_60d"], 2, '.', '')."%</p>
+                                <p>200D: ".number_format($coin["price_change_200d"], 2, '.', '')."%</p>
+                                <p>1Y: ".number_format($coin["price_change_1yr"], 2, '.', '')."%</p>
                             </div>
-                                <div class=\"product-image-mask\">
-                                <div class=\"product-image\" style=\"background-image: url(".$coin["img_url"].");\"></div>
-                            </div><div class=\"product-info-container\">
-                                <h3>".$coin["name"]."</h3>
-                                <strong class=\"product-price\">".number_format($coin[$dashboardModules[$currentCount]["fiat"]], 4, '.', '')." ".strtoupper($dashboardModules[$currentCount]["fiat"])." ".number_format($coin["price_change_24h"], 2, '.', '')."%</strong>
-                                <div class=\"price-trend-container\">
-                                    <p>7D: ".number_format($coin["price_change_7d"], 2, '.', '')."%</p>
-                                    <p>14D: ".number_format($coin["price_change_14d"], 2, '.', '')."%</p>
-                                    <p>30D: ".number_format($coin["price_change_30d"], 2, '.', '')."%</p>
-                                    <p>60D: ".number_format($coin["price_change_60d"], 2, '.', '')."%</p>
-                                    <p>200D: ".number_format($coin["price_change_200d"], 2, '.', '')."%</p>
-                                    <p>1Y: ".number_format($coin["price_change_1yr"], 2, '.', '')."%</p>
-                                </div>
-                            </div>
-                        </div>";
-                    }
+                        </div>
+                    </div>";
+                }
                 echo "</div>";
   
             ?>
