03a4a7730a879853918a8965e4c0d2295395e180
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-02 21:40:09 -0700

    Admin Page v0.1

--- php/admin.php
+++ php/admin.php
@@ -1,111 +1,89 @@
-<?php
-    session_start();
-    include "DBconnection.php";
-    // include "updateUser.php";
-
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    }else{
-
-        if(isset($_POST['searchByName'])){
-            $searchByName = $_POST['searchByName'];
-            //Search user by name
-            $selectUserByNamestmt = $con->prepare("SELECT * FROM user_auth WHERE Username = ?");
-            $selectUserByNamestmt->bind_param("s", $searchByName); 
-            $selectUserByNamestmt->execute();
-            $resultSetselectUserByNamestmt = $selectUserByNamestmt->get_result(); // get the mysqli result
-            $resultselectUserByName = $resultSetselectUserByNamestmt->fetch_assoc();
-        }
-
-        if(isset($_POST['searchByEmail'])){
-            $searchByEmail = $_POST['searchByEmail'];
-            // //Search user by email
-            $selectUserByEmailstmt = $con->prepare("SELECT * FROM user_auth WHERE Email = ?");
-            $selectUserByEmailstmt->bind_param("s", $searchByEmail); 
-            $selectUserByEmailstmt->execute();
-            $resultSetselectUserByEmailstmt = $selectUserByEmailstmt->get_result(); // get the mysqli result
-            $resultselectUserByEmail = $resultSetselectUserByEmailstmt->fetch_assoc();
-            
-        }
-
-        if(isset($_POST['searchByCommentId'])){
-            $searchByCommentId = $_POST['searchByCommentId'];
-            // //Search user by comment
-            $selectUserByCommentstmt = $con->prepare("SELECT * FROM user_auth AS u JOIN comment AS c ON c.user_id = u.Id WHERE c.id = ?");
-            $selectUserByCommentstmt->bind_param("i", $searchByCommentId); 
-            $selectUserByCommentstmt->execute();
-            $resultSetselectUserByCommentstmt = $selectUserByCommentstmt->get_result(); // get the mysqli result
-            $resultselectUserByComment = $resultSetselectUserByCommentstmt->fetch_assoc();
-        }
-
-
-    }
-
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Page</title>
-    <link rel="stylesheet" href="../font/helvetica-now-display/stylesheet.css">
-    <link rel="stylesheet" href="../css/var.css">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/header-footer.css">
-    <link rel="stylesheet" href="../css/module.css">
-    <link rel="stylesheet" href="../css/individual.css">
-    <script src="https://kit.fontawesome.com/e6e0351429.js" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="../js/jquery-3.1.1.min.js"></script>
-    <style>
-        .divider{
-            margin-bottom: 2em;
-        }
-        
-        #commentText{
-            width: 118em;
-        }
-    </style>    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial;}
+
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+  font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+}
+</style>
 </head>
 <body>
-    <?php
-        include "dashboard-header.php";
-    ?> 
-<main>    
-    <article class="panel">
-        <form action="" method="POST">
-            Search by Name: <input type="text" name ="searchByName" id="searchByName"><input type="submit" name="submit" value="search">
-            <div class="reviews divider">
-                <h2>User Information</h2>
-                <p>User Id : <?php echo $resultselectUserByName['Id']?></p>
-                <p>User Username : <?php echo $resultselectUserByName['Username']?></p>
-                <p>User Email : <?php echo $resultselectUserByName['Email']?></p>
-                <p>User Status : <?php echo $resultselectUserByName['status']?></p>
-            </div>
 
-            Search by Email: <input type="text" name ="searchByEmail" id="searchByEmail"><input type="submit" name="submit" value="search">
-            <div class="reviews divider">
-                <h2>User Information</h2>
-                <p>User Id : <?php echo $resultselectUserByEmail['Id']?></p>
-                <p>User Username : <?php echo $resultselectUserByEmail['Username']?></p>
-                <p>User Email : <?php echo $resultselectUserByEmail['Email']?></p>
-                <p>User Status : <?php echo $resultselectUserByEmail['status']?></p>
-            </div>
+<h2>Tabs</h2>
+<p>Click on the buttons inside the tabbed menu:</p>
 
-            Search by Comment ID: <input type="text" name ="searchByCommentId" id="searchByCommentId"><input type="submit" name="submit" value="search">
-            <div class="reviews">
-                <h2>User Information</h2>
-                <p>User Id : <?php echo $resultselectUserByComment['Id']?></p>
-                <p>User Username : <?php echo $resultselectUserByComment['Username']?></p>
-                <p>User Email : <?php echo $resultselectUserByComment['Email']?></p>
-                <p>User Status : <?php echo $resultselectUserByComment['status']?></p>                
-                <input type="text" name ="commentText" id="commentText" value="<?php echo $resultselectUserByComment['text']?>">
-                <input type="submit" name="submit" value="change">
-            </div>
-        </form>
-    </article>
-</main>    
-</main>
+<div class="tab">
+  <button class="tablinks" onclick="openCity(event, 'London')">London</button>
+  <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+  <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+</div>
+
+<div id="London" class="tabcontent">
+  <h3>London</h3>
+  <p>London is the capital city of England.</p>
+</div>
+
+<div id="Paris" class="tabcontent">
+  <h3>Paris</h3>
+  <p>Paris is the capital of France.</p> 
+</div>
+
+<div id="Tokyo" class="tabcontent">
+  <h3>Tokyo</h3>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+
+<script>
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
+   
 </body>
-</html>
+</html> 
