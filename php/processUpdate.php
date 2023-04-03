36f30b8c950a6ec962821dde5d744ec108c58a5e
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-02 20:58:17 -0700

    Reformatting code v0.1

--- php/processUpdate.php
+++ php/processUpdate.php
@@ -1,12 +1,13 @@
 <?php
     include "modules.php";
+    echo "Updated 1!";
     $errMsg = '';
     if(isset($_GET['updateSubmit']) && $_SERVER["REQUEST_METHOD"] == "POST"){
         if(isset($_GET['email']) && isset($_GET['password'])){
             $userEmail= $_GET['email'];
             $userPassword = $_GET['password'];
             updateUser($con,$userEmail,$userPassword);
-            echo "Updated !";
+            echo "Updated 2!";
         }else{
             $errMsg = 'Login data was not sent. Please try again !';
             echo "<script>console.log(\"".$errMsg."\")</script>";
