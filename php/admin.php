a7feca4dee4452f57be7200ebef6b2511f92d125
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-03 15:30:00 -0700

    Admin Page v0.1

--- php/admin.php
+++ php/admin.php
@@ -3,47 +3,39 @@
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../js/admin.js"></script>
     </head>
         <body>
-            <h2>Tabs</h2>
-            <p>Click on the buttons inside the tabbed menu:</p>
-
-            <div class="tab">
-            <button class="tablinks" onclick="openCity(event, 'London')">By Username</button>
-            <button class="tablinks" onclick="openCity(event, 'Paris')">By Email ID</button>
-            <button class="tablinks" onclick="openCity(event, 'Tokyo')">By Comment ID</button>
+            <h2>Admin Dashboard</h2>
+            <div class="tabswitcher">
+                                     
+                <div class="tab">
+                    <button class="tablinks" onclick="openCity(event, 'username')" id="defaultOpen">By Username</button>
+                    <button class="tablinks" onclick="openCity(event, 'email')">By Email ID</button>
+                    <button class="tablinks" onclick="openCity(event, 'commentId')">By Comment ID</button>
+                </div>
+                
+                <div id="username" class="tabcontent" >
+                    <form class="example" action="processUserInfo.php" method = "POST">
+                        <input type="text" placeholder="Search.." name="username">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                      </form>
+                </div>
+                
+                <div id="email" class="tabcontent">
+                    <form class="example" action="processUserInfo.php" method = "POST">
+                        <input type="text" placeholder="Search.." name="email">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                      </form>
+                </div>
+                
+                <div id="commentId" class="tabcontent">
+                    <form class="example" action="processUserInfo.php" method = "POST">
+                        <input type="text" placeholder="Search.." name="commentId">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                      </form>
+                </div>
             </div>
-
-            <div id="London" class="tabcontent">
-            <h3>London</h3>
-            <p>London is the capital city of England.</p>
-            </div>
-
-            <div id="Paris" class="tabcontent">
-            <h3>Paris</h3>
-            <p>Paris is the capital of France.</p> 
-            </div>
-
-            <div id="Tokyo" class="tabcontent">
-            <h3>Tokyo</h3>
-            <p>Tokyo is the capital of Japan.</p>
-            </div>
-
-            <script>
-                function openCity(evt, cityName) {
-                    var i, tabcontent, tablinks;
-                    tabcontent = document.getElementsByClassName("tabcontent");
-                    for (i = 0; i < tabcontent.length; i++) {
-                        tabcontent[i].style.display = "none";
-                    }
-                    tablinks = document.getElementsByClassName("tablinks");
-                    for (i = 0; i < tablinks.length; i++) {
-                        tablinks[i].className = tablinks[i].className.replace(" active", "");
-                    }
-                    document.getElementById(cityName).style.display = "block";
-                    evt.currentTarget.className += " active";
-                }
-            </script>
-        
         </body>
 </html> 
