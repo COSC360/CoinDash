ed2df4991f4558ec1871c434908ae37b64c76aaf
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 16:41:16 -0700

    Fixing Admin v0.1

--- php/userInfoDisplay.php
+++ php/userInfoDisplay.php
@@ -11,7 +11,7 @@
         <button id = "editUserBtn">Edit</button>
         <button id = "saveUserBtn" type ="submit" form="infoDisplayForm" name="saveUser">Save</button>
     </div>
-    <form id ="infoDisplayForm" method = "POST" action="processUpdate.php">
+    <!-- <form id ="infoDisplayForm" method = "POST" action="processUpdate.php"> -->
         <div id = "item-2">
             <label>User ID :</label>
             <input type="text" name="userID" value = "<?php echo $_SESSION['RSId']?>">
@@ -30,7 +30,7 @@
             <label>User Registeration Timestamp :</label>
             <input type="text" name="regTimestamp" value = "<?php echo $_SESSION['RSRegisterTimestamp']?>" >
         </div>
-    <form>    
+    <!-- <form>     -->
 </div>
 <script>
     const infoDisplayForm = document.forms["infoDisplayForm"];
