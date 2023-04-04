b8221193236974c69d9e194185053d932cafee84
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 15:14:27 -0700

    Fixing Admin v0.1

--- php/sql-queries.php
+++ php/sql-queries.php
@@ -317,7 +317,6 @@ function loginUser($con,$loginID,$loginPassword){
         }elseif($rows['userType'] == 'user' && $rows['status'] == "disabled"){
             $statusMsg = "Your account has been disabled by the admin !";
             header('location:signIn.php');
-            sleep(2);
             echo "<script>window.alert(\"".$statusMsg."\")</script>";
         }
     }else{
