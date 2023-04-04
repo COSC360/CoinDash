cf09e4ed9dc65c071e6071cdea1509fade397e01
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 14:12:48 -0700

    Fixing Admin v0.1

--- php/userInfoDisplay.php
+++ php/userInfoDisplay.php
@@ -1,36 +1,47 @@
 <?php
     session_start();
 ?>
-<div class="userInfo">
-    <div id = "item-1">
-        <img src="<?php echo $_SESSION['RSProfilePicture']?>" id = "pfp">
-        <button class = "enableUserBtn">Enable</button>
-        <button class = "disableUserBtn">Disable</button>
-        <button class = "deleteUserBtn">Delete</button>
-        <button class = "editUserBtn">Edit</button>
-    </div>
-    <form id ="infoDisplayForm">
-        <div id = "item-2">
-            <label>User ID :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSId']?>" >
-            <label>Username :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSUsername']?>" >
-            <label>User Password :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSPassword']?>" >
-            <label>User Email :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSEmail']?>" >
-            <label>User Coming From :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSComingFrom']?>" >
-            <label>User Type :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSUserType']?>" >
-            <label>User Status :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSUserStatus']?>" >
-            <label>User Registeration Timestamp :</label>
-            <input type="text" value = "<?php echo $_SESSION['RSRegisterTimestamp']?>" >
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="userInfo">
+        <div id = "item-1">
+            <img src="<?php echo $_SESSION['RSProfilePicture']?>" id = "pfp">
+            <button class = "enableUserBtn">Enable</button>
+            <button class = "disableUserBtn">Disable</button>
+            <button class = "deleteUserBtn">Delete</button>
+            <button class = "editUserBtn">Edit</button>
         </div>
-    <form>    
-</div>
-<script>
-    const infoDisplayForm = document.querySelectorAll('infoDisplayForm');
-    console.log(infoDisplayForm)
-</script>
+        <form id ="infoDisplayForm">
+            <div id = "item-2">
+                <label>User ID :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSId']?>" >
+                <label>Username :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSUsername']?>" >
+                <label>User Password :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSPassword']?>" >
+                <label>User Email :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSEmail']?>" >
+                <label>User Coming From :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSComingFrom']?>" >
+                <label>User Type :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSUserType']?>" >
+                <label>User Status :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSUserStatus']?>" >
+                <label>User Registeration Timestamp :</label>
+                <input type="text" value = "<?php echo $_SESSION['RSRegisterTimestamp']?>" >
+            </div>
+        <form>    
+    </div>
+    <script>
+        const infoDisplayForm = document.querySelectorAll('infoDisplayForm');
+        console.log(infoDisplayForm)
+    </script>    
+</body>
+</html>
