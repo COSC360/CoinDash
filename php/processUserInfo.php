0892fd22151ab661ff4495d963befc4e8389dcff
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-03 18:40:26 -0700

    Admin Page v0.3

--- php/processUserInfo.php
+++ php/processUserInfo.php
@@ -3,23 +3,19 @@
     
     $errMsg = '';
     echo "On PHP Page<br>";
-    if(isset($_SERVER['submitName']) && $_SERVER["REQUEST_METHOD"] == "POST"){
-        echo "Inside Loop 1<br>";
+    if(isset($_POST['submitName']) && $_SERVER["REQUEST_METHOD"] == "POST"){
         if(isset($_POST['searchName'])){
-            echo "Inside Loop 2<br>";
             $searchName = $_POST['searchName'];
             searchByUsername($con,$searchName);
-            echo "Function Worked<br>";
         }else{
-            echo "Inside Error Loop";
             $errMsg = 'Login data was not sent. Please try again !';
             echo "<script>console.log(\"".$errMsg."\")</script>";
         }
     }else{
-        echo "Failed<br>";
+        echo "Failed !";
     }
 
-    if(isset($_SERVER['submitEmail']) && $_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST['submitEmail']) && $_SERVER["REQUEST_METHOD"] == "POST"){
         if(isset($_POST['searchEmail'])){
             $searchEmail = $_POST['searchEmail'];
             searchByEmail($con,$searchEmail);
@@ -29,7 +25,7 @@
         }
     }
     
-    if(isset($_SERVER['submitCommentId']) && $_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST['submitCommentId']) && $_SERVER["REQUEST_METHOD"] == "POST"){
         if(isset($_POST['searchCommentId'])){
             $searchCommentId = $_POST['searchCommentId'];
             searchByCommentId($con, $searchCommentId);
