01f96a9462f42cda1d35385dede2c1e85cc7b89c
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 15:05:04 -0700

    Fixing Admin v0.1

--- php/processUpdate.php
+++ php/processUpdate.php
@@ -2,7 +2,7 @@
     include "modules.php";
     
     $errMsg = '';
-    echo isset($_POST['disableUser']);
+    echo isset($_POST['enableUser']);
 
     if(isset($_POST['updateSubmit']) && $_SERVER["REQUEST_METHOD"] == "POST"){      
         if(isset($_POST['email']) && isset($_POST['password'])){
