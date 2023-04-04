1bef5d2cb25c22c4004635a65918e56f66b4788c
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 15:18:03 -0700

    Fixing Admin v0.1

--- php/signIn.php
+++ php/signIn.php
@@ -2,6 +2,7 @@
     session_start();
     if(isset($_SESSION['statusMsg'])){
         echo "<script>window.alert(\"".$_SESSION['statusMsg']."\")</script>";
+        session_destroy();
     }else{
         session_destroy();
     }
