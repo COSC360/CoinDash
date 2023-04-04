65fe42dc40e24a59d9d2c7be573446807da44432
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-03 19:46:13 -0700

    Admin Page v0.3

--- php/adminlogin.php
+++ php/adminlogin.php
@@ -8,24 +8,15 @@
     <title>Admin Login</title>
 </head>
 <body>
-    <div class="back">
-        <div class="div-center">
-            <div class="content">
-                <h3>Admin Login</h3>
-                <hr/>
-                <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
-            </div>
-        </div>
+    <div class="form-container">
+    <h1>Admin Login</h1>
+        <section>
+            <input #username type="text" name="username" placeholder="Username">
+            <br />
+            <input #password type="password" name="password" placeholder="Password">
+            <br />
+            <button type="button" role="button" (click)="login($event, username, password)">Login</button>
+        </section>
     </div>
 </body>
 </html>
