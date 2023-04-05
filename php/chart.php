1ca9237bab4eeace3c9ef3283badea1a38af593d
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 10:40:46 -0700

    Fixing Admin v0.1

--- php/chart.php
+++ php/chart.php
@@ -3,29 +3,40 @@
   include 'modules.php';
 
   retrieveRegSourceChartData($con);
+  retrieveCommentCountChartData($con);  
 ?>  
   <canvas id="doughnut"></canvas>
+  <canvas id="bar"></canvas>
   <?php
   echo
   "<script>
 
     var xValues = [];
-    var yValues = [];";
+    var yValues = [];
+    var xCommentValues = [];
+    var yCommentValues = [];";
     foreach($_SESSION['dataArray'] as $row){
         echo "xValues.push(\"".$row."\");";
     }
     foreach($_SESSION['countDataArray'] as $row){
         echo "yValues.push(\"".$row."\");";
     }
+    foreach($_SESSION['commentDataArray'] as $row){
+      echo "xCommentValues.push(\"".$row."\");";
+    }
+    foreach($_SESSION['commentCountDataArray'] as $row){
+        echo "yCommentValues.push(\"".$row."\");";
+    }
   echo 
-    "var barColors = [\"#00aba9\", \"#2b5797\",\"#b91d47\"];
+    "
+    var chartColors = [\"#00aba9\", \"#2b5797\",\"#b91d47\",\"#e8c3b9\",\"#1e7145\",\"#993366\",\"#009999\"];
 
     new Chart(\"doughnut\", {
       type: \"doughnut\",
       data: {
         labels: xValues,
         datasets: [{
-          backgroundColor: barColors,
+          backgroundColor: chartColors,
           data: yValues
         }]
       },
@@ -34,6 +45,24 @@
         title: {
           display: true,
           text: \"User Registeration Source\"
+        }
+      }
+    });
+
+    new Chart(\"bar\", {
+      type: \"bar\",
+      data: {
+        labels: xCommentValues,
+        datasets: [{
+          backgroundColor: chartColors,
+          data: yCommentValues
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: \"User Comment Activity\"
         }
       }
     });
