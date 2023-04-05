2c014516a05b0b729c2755473ebb06b7084ae7ac
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 17:02:04 -0700

    Fixing Admin v0.1

--- php/userInfoDisplay.php
+++ php/userInfoDisplay.php
@@ -48,7 +48,7 @@
         for(var i = 0; i < infoDisplayForm.length; i++){
             infoDisplayForm[i].disabled = false;
         }
-        enableUserBtn.setAttribute("background-color", "#ffffff");
+        enableUserBtn.setAttribute(background-color, "red");
     }
 
 
