11d74f393d98009ff733ad9e6482c21a56773730
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 22:41:27 -0700

    Fixing Admin v0.1

--- php/chart.php
+++ php/chart.php
@@ -11,44 +11,42 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 </head>
 <body>
+  <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+  <?php
+  echo
+  "<script>
 
-<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
-<?php
-echo
-"<script>
-
-  var xValues = [];
-  var yValues = [];";
-  foreach($_SESSION['dataArray'] as $row){
-      echo "xValues.push(\"".$row."\");";
-  }
-  foreach($_SESSION['countDataArray'] as $row){
-      echo "yValues.push(\"".$row."\");";
-  }
-echo 
-  "
-  console.log(xValues);
-  console.log(yValues);
-  var barColors = [\"red\", \"green\",\"blue\"];
-
-  new Chart(\"myChart\", {
-    type: \"bar\",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
-    },
-    options: {
-      legend: {display: false},
-      title: {
-        display: true,
-        text: \"World Wine Production 2018\"
-      }
+    var xValues = [];
+    var yValues = [];";
+    foreach($_SESSION['dataArray'] as $row){
+        echo "xValues.push(\"".$row."\");";
     }
-  });
-</script>";
-?>
+    foreach($_SESSION['countDataArray'] as $row){
+        echo "yValues.push(\"".$row."\");";
+    }
+  echo 
+    "
+    console.log(xValues);
+    console.log(yValues);
+    var barColors = [\"red\", \"green\",\"blue\"];
 
+    new Chart(\"myChart\", {
+      type: \"bar\",
+      data: {
+        labels: xValues,
+        datasets: [{
+          backgroundColor: barColors,
+          data: yValues
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: \"World Wine Production 2018\"
+        }
+      }
+    });
+  </script>";
+  ?>
 </body>
