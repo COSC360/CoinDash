f879444106e295af4fccd633499a8f23058edabe
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 10:36:27 -0700

    Fixing Admin v0.1

--- php/chart.php
+++ php/chart.php
@@ -3,31 +3,29 @@
   include 'modules.php';
 
   retrieveRegSourceChartData($con);
-  retrieveCommentCountChartData($con);
-
+?>  
+  <canvas id="doughnut"></canvas>
+  <?php
   echo
-  "
-  <canvas id=\"doughnut\"></canvas>
-  
-  <script>
+  "<script>
 
-    var xRegSourceValues = [];
-    var yRegSourceValues = [];";
-    foreach($_SESSION['regSourceDataArray'] as $row){
-        echo "xRegSourceValues.push(\"".$row."\");";
+    var xValues = [];
+    var yValues = [];";
+    foreach($_SESSION['dataArray'] as $row){
+        echo "xValues.push(\"".$row."\");";
     }
-    foreach($_SESSION['regSourceCountDataArray'] as $row){
-        echo "yRegSourceValues.push(\"".$row."\");";
+    foreach($_SESSION['countDataArray'] as $row){
+        echo "yValues.push(\"".$row."\");";
     }
   echo 
-    "var doughnutColors = [\"#00aba9\", \"#2b5797\",\"#b91d47\", \"#1e7145\",\"#e8c3b9\",\"#063970\",\"#48249c\"];
+    "var barColors = [\"#00aba9\", \"#2b5797\",\"#b91d47\"];
 
     new Chart(\"doughnut\", {
       type: \"doughnut\",
       data: {
         labels: xValues,
         datasets: [{
-          backgroundColor: doughnutColors,
+          backgroundColor: barColors,
           data: yValues
         }]
       },
