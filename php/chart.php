a1294a4445409f2b94848eb31fcaac6dafbeaf27
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 14:43:21 -0700

    Building Admin Page v0.4

--- php/chart.php
+++ php/chart.php
@@ -56,7 +56,7 @@
         }]
       },
       options: {
-        legend: {display: true},
+        legend: {display: true, position: right},
         title: {
           display: true,
           text: \"User Registeration Source\"
@@ -74,7 +74,7 @@
         }]
       },
       options: {
-        legend: {display: false},
+        legend: {display: true, position: right},
         title: {
           display: true,
           text: \"User Comment Activity\"
@@ -92,7 +92,7 @@
         }]
       },
       options: {
-        legend: {display: true},
+        legend: {display: true, position: right},
         title: {
           display: true,
           text: \"User Status Data\"
