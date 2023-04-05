6844f77b244079b571cf208f91c64d066ef40ccd
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 17:38:54 -0700

    Fixing Admin v0.1

--- php/userInfoDisplay.php
+++ php/userInfoDisplay.php
@@ -18,7 +18,7 @@
             <label>Username :</label>
             <input type="text" name="username" value = "<?php echo $_SESSION['RSUsername']?>">
             <label>User Password :</label>
-            <input type="text" name="password" value = "<?php echo md5($_SESSION['RSPassword'])?>">
+            <input type="text" name="password" value = "<?php echo $_SESSION['RSPassword']?>">
             <label>User Email :</label>
             <input type="text" name="email" value = "<?php echo $_SESSION['RSEmail']?>">
             <label>User Coming From :</label>
