5ec7f03b584f3282ace284be743f126462a9c33a
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 19:25:49 -0700

    Fixing Admin v0.1

--- php/userInfoDisplay.php
+++ php/userInfoDisplay.php
@@ -31,20 +31,5 @@
             <input type="text" name="regTimestamp" value = "<?php echo $_SESSION['RSRegisterTimestamp']?>">
         </div>
     </form>    
-</div>
-<script>
-    for(var i = 0; i < infoDisplayForm.length; i++){
-        infoDisplayForm[i].disabled = true;
-    }
-    
-    function enableField(){
-        for(var i = 0; i < infoDisplayForm.length; i++){
-            infoDisplayForm[i].disabled = false;
-        }
-        enableUserBtn.setAttribute("style", "background-color: #2fc363;");
-        disableUserBtn.setAttribute("style", "background-color: #2fc363;");
-        deleteUserBtn.setAttribute("style", "background-color: #2fc363;");
-        saveUserBtn.setAttribute("style", "background-color: #2fc363;");
-    }
-</script>    
+</div>  
 
