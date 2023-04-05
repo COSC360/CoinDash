aff862eeeeaee9764f465a2270ae9d48a8de342a
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 21:33:16 -0700

    Fixing Admin v0.1

--- php/admin.php
+++ php/admin.php
@@ -25,6 +25,7 @@
     <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../js/admin.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
     <title>Admin</title>
 </head>
     <body>
