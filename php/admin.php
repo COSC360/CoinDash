684268aead86e9d178663802446aed81df18147d
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 19:48:00 -0700

    Fixing Admin v0.1

--- php/admin.php
+++ php/admin.php
@@ -66,31 +66,27 @@
             <script>
                 const infoDisplayForm = document.forms;
 
-                const searchByNameForm = infoDisplayForm[2];
-                const searchByEmailForm = infoDisplayForm[4];
-                const searchByCommentIdForm = infoDisplayForm[6];
-                
-                console.log(searchByNameForm[0]);
-                console.log(infoDisplayForm[2][0]);
-                // document.getElementById("editUserBtn").addEventListener("click", enableField);
-                // const enableUserBtn = document.getElementById("enableUserBtn");
-                // const disableUserBtn = document.getElementById("disableUserBtn");
-                // const deleteUserBtn = document.getElementById("deleteUserBtn");
-                // const saveUserBtn = document.getElementById("saveUserBtn");
+                for(var i = 2; i <= 6; i = i + 2){
+                    document.getElementById("editUserBtn").addEventListener("click", enableField);
+                    const enableUserBtn = document.getElementById("enableUserBtn");
+                    const disableUserBtn = document.getElementById("disableUserBtn");
+                    const deleteUserBtn = document.getElementById("deleteUserBtn");
+                    const saveUserBtn = document.getElementById("saveUserBtn");
 
-                // for(var i = 0; i < infoDisplayForm.length; i++){
-                //     infoDisplayForm[i].disabled = true;
-                // }
-                
-                // function enableField(){
-                //     for(var i = 0; i < infoDisplayForm.length; i++){
-                //         infoDisplayForm[i].disabled = false;
-                //     }
-                //     enableUserBtn.setAttribute("style", "background-color: #2fc363;");
-                //     disableUserBtn.setAttribute("style", "background-color: #2fc363;");
-                //     deleteUserBtn.setAttribute("style", "background-color: #2fc363;");
-                //     saveUserBtn.setAttribute("style", "background-color: #2fc363;");
-                // }
+                    for(var j = 0; j < infoDisplayForm[i].length; j++){
+                        infoDisplayForm[i][j].disabled = true;
+                    }
+                    
+                    function enableField(){
+                        for(var j = 0; j < infoDisplayForm.length; j++){
+                            infoDisplayForm[i][j].disabled = false;
+                        }
+                        enableUserBtn.setAttribute("style", "background-color: #2fc363;");
+                        disableUserBtn.setAttribute("style", "background-color: #2fc363;");
+                        deleteUserBtn.setAttribute("style", "background-color: #2fc363;");
+                        saveUserBtn.setAttribute("style", "background-color: #2fc363;");
+                    }
+                }
 
                 document.getElementById("<?php echo $_SESSION['defaultTabID']?>").click();
             </script>
