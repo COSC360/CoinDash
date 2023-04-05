3ec089a2c87ca029b27f65cf5273c14d83e4aaf2
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-04 20:03:52 -0700

    Fixing Admin v0.1

--- php/admin.php
+++ php/admin.php
@@ -65,24 +65,24 @@
             </div>
             <script>
                 const infoDisplayForm = document.forms;
-               
-                for(var i = 2; i <= 6; i = i + 2){
-                    document.getElementById(infoDisplayForm[i][0].getAttribute("id")).addEventListener("click", enableField);
+                console.log(infoDisplayForm[2].length)
+                // for(var i = 2; i <= 6; i = i + 2){
+                //     document.getElementById(infoDisplayForm[i][0].getAttribute("id")).addEventListener("click", enableField);
 
-                    for(var j = 0; j < infoDisplayForm[i].length; j++){
-                        infoDisplayForm[i][j].disabled = true;
-                    }
+                //     for(var j = 0; j < infoDisplayForm[i].length; j++){
+                //         infoDisplayForm[i][j].disabled = true;
+                //     }
                     
-                    function enableField(){
-                        for(var j = 0; j < infoDisplayForm[i].length; j++){
-                            infoDisplayForm[i][j].disabled = false;
-                        }
-                        document.getElementById(infoDisplayForm[i][0].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                        document.getElementById(infoDisplayForm[i][1].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                        document.getElementById(infoDisplayForm[i][2].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                        document.getElementById(infoDisplayForm[i][3].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                    }
-                }
+                //     function enableField(){
+                //         for(var j = 0; j < infoDisplayForm[i].length; j++){
+                //             infoDisplayForm[i][j].disabled = false;
+                //         }
+                //         document.getElementById(infoDisplayForm[i][0].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
+                //         document.getElementById(infoDisplayForm[i][1].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
+                //         document.getElementById(infoDisplayForm[i][2].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
+                //         document.getElementById(infoDisplayForm[i][3].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
+                //     }
+                // }
 
                 document.getElementById("<?php echo $_SESSION['defaultTabID']?>").click();
             </script>
