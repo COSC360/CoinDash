1f98957eb78658942fcee4d4e0798f9dae002693
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 14:32:30 -0700

    Building Admin Page v0.4

--- php/adminAnalytics.php
+++ php/adminAnalytics.php
@@ -1,4 +1,25 @@
-<?php 
-    include 'chart.php';
+<?php
+    session_start();
 ?>
-                    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/var.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/header-footer.css">
+    <link rel="stylesheet" href="../css/module.css">
+    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../js/admin.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+    <title>Admin Analytics</title>
+</head>
+<body>
+    <?php 
+        include 'chart.php';
+    ?>    
+</body>
+</html>
+                 
