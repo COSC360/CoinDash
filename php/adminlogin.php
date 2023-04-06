ad826fa1e7311ad851c519826dba7321ff8d068e
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 20:37:56 -0700

    Added title logo(new)

--- php/adminlogin.php
+++ php/adminlogin.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/adminlogin.css">
+    <link rel="icon" href="../images/sitelogo.png" type="image/icon type">
     <title>Admin Login</title>
 </head>
 <body>
