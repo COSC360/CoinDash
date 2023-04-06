b7698935ee848bd447df3a1a52b1bab35ed96e28
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 17:22:19 -0700

    Building Admin Page v0.4

--- php/admin.php
+++ php/admin.php
@@ -82,56 +82,43 @@
                 const displayByEmail = document.forms['displayByEmail'];
                 const displayByCommentId = document.forms['displayByCommentId'];
 
-                // document.querySelectorAll('[id=editUserBtn]')[0].addEventListener("click", byUsernameEnableField);
-                // document.querySelectorAll('[id=editUserBtn]')[1].addEventListener("click", byEmailEnableField);
-                // document.querySelectorAll('[id=editUserBtn]')[2].addEventListener("click", byCommentIdEnableField);
+                document.querySelectorAll('[id=editUserBtn]')[0].addEventListener("click", byUsernameEnableField);
+                document.querySelectorAll('[id=editUserBtn]')[1].addEventListener("click", byEmailEnableField);
+                document.querySelectorAll('[id=editUserBtn]')[2].addEventListener("click", byCommentIdEnableField);
 
-                console.log(document.querySelectorAll('[id=enableUserBtn]'));
-                console.log(document.querySelectorAll('[id=disableUserBtn]'));
-                console.log(document.querySelectorAll('[id=deleteUserBtn]'));
-                console.log(document.querySelectorAll('[id=saveUserBtn]'));
-                // for(var i = 0; i < displayByName.length; i++){
-                //     displayByName[i].disabled = true;
-                //     displayByEmail[i].disabled = true;
-                //     displayByCommentId[i].disabled = true;
-                // }
+                for(var i = 0; i < displayByName.length; i++){
+                    displayByName[i].disabled = true;
+                    displayByEmail[i].disabled = true;
+                    displayByCommentId[i].disabled = true;
+                }
                 
-                // function byUsernameEnableField(){               
-                //     for(var i = 0; i < displayByName.length; i++){
-                //         displayByName[i].disabled = false;
-                //     }
-
-                //     for(var i = 0; i < 4; i++){
-                //         document.getElementById(displayByName[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByName[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByName[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByName[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //     }
-                // }
-                // function byEmailEnableField(){                 
-                //     for(var i = 0; i < displayByEmail.length; i++){
-                //         displayByEmail[i].disabled = false;
-                //     }
-
-                //     for(var i = 0; i < 4; i++){
-                //         document.getElementById(displayByEmail[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByEmail[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByEmail[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByEmail[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //     }
-                // }
-                // function byCommentIdEnableField(){                 
-                //     for(var i = 0; i < displayByCommentId.length; i++){
-                //         displayByCommentId[i].disabled = false;
-                //     }
-
-                //     for(var i = 0; i < 4; i++){
-                //         document.getElementById(displayByCommentId[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByCommentId[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByCommentId[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //         document.getElementById(displayByCommentId[i].getAttribute("id")).setAttribute("style", "background-color: #2fc363;");
-                //     }
-                // }
+                function byUsernameEnableField(){               
+                    for(var i = 0; i < displayByName.length; i++){
+                        displayByName[i].disabled = false;
+                    }
+                    document.querySelectorAll('[id=enableUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=disableUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=deleteUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=saveUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+                }
+                function byEmailEnableField(){                 
+                    for(var i = 0; i < displayByEmail.length; i++){
+                        displayByEmail[i].disabled = false;
+                    }   
+                    document.querySelectorAll('[id=enableUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=disableUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=deleteUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=saveUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");         
+                }
+                function byCommentIdEnableField(){                 
+                    for(var i = 0; i < displayByCommentId.length; i++){
+                        displayByCommentId[i].disabled = false;
+                    }                    
+                    document.querySelectorAll('[id=enableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=disableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=deleteUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+                    document.querySelectorAll('[id=saveUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+                }
 
                 document.getElementById("<?php echo $_SESSION['defaultTabID']?>").click();
             </script>
