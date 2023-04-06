c71e55811aa53510618e73782ab967d6bf0d558f
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 14:51:35 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -8,7 +8,7 @@ const passwordRegex = "^[a-zA-Z]\w{8,16}$ "; //Simple password expression. The p
 const emailRegex = "^(.+)@([^\.].*)\.([a-z]{2,})$"; //Email validation based on current standard naming rules
 var errMsg = "";
 
-console.log(loginForm);
+window.alert(loginForm);
 
 // loginForm.addEventListener('submit', (event) => {
 //     alert('submitting');
