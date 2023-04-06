ac21d69ca4c194597345fde0d096c3538940de7d
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 13:29:26 -0700

    Editting Header

--- php/testURI.php
+++ php/testURI.php
@@ -1,11 +1,11 @@
 <?php
-    $uri = $_SERVER['REQUEST_URI'];
-    echo $uri."<br>"; // Outputs: URI
+    // $uri = $_SERVER['REQUEST_URI'];
+    // echo $uri."<br>"; // Outputs: URI
 
-    $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    // $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
  
-    $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    echo $url."<br>"; // Outputs: Full URL
+    // $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    // echo $url."<br>"; // Outputs: Full URL
 
     $query = $_SERVER['QUERY_STRING'];
     echo $query."<br>"; // Outputs: Query String
