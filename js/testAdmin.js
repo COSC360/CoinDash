1e5049ad49fa204cd18bca7932b60feb3f6c67c8
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 17:24:19 -0700

    Building Admin Page v0.4

---
+++ js/testAdmin.js
@@ -0,0 +1,43 @@
+const displayByName = document.forms['displayByName'];
+const displayByEmail = document.forms['displayByEmail'];
+const displayByCommentId = document.forms['displayByCommentId'];
+
+document.querySelectorAll('[id=editUserBtn]')[0].addEventListener("click", byUsernameEnableField);
+document.querySelectorAll('[id=editUserBtn]')[1].addEventListener("click", byEmailEnableField);
+document.querySelectorAll('[id=editUserBtn]')[2].addEventListener("click", byCommentIdEnableField);
+
+for(var i = 0; i < displayByName.length; i++){
+    displayByName[i].disabled = true;
+    displayByEmail[i].disabled = true;
+    displayByCommentId[i].disabled = true;
+}
+
+function byUsernameEnableField(){               
+    for(var i = 0; i < displayByName.length; i++){
+        displayByName[i].disabled = false;
+    }
+    document.querySelectorAll('[id=enableUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=disableUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=deleteUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=saveUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
+}
+function byEmailEnableField(){                 
+    for(var i = 0; i < displayByEmail.length; i++){
+        displayByEmail[i].disabled = false;
+    }   
+    document.querySelectorAll('[id=enableUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=disableUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=deleteUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=saveUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");         
+}
+function byCommentIdEnableField(){                 
+    for(var i = 0; i < displayByCommentId.length; i++){
+        displayByCommentId[i].disabled = false;
+    }                    
+    document.querySelectorAll('[id=enableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=disableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=deleteUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+    document.querySelectorAll('[id=saveUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+}
+
+document.getElementById("<?php echo $_SESSION['defaultTabID']?>").click();
--- php/admin.php
+++ php/admin.php
@@ -25,6 +25,7 @@
     <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../js/admin.js"></script>
+    <script src="../js/testAdmin.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
     <title>Admin</title>
 </head>
@@ -77,51 +78,8 @@
                 <div class = "commentData">
                 </div>
             </div>
-            <script>
-                const displayByName = document.forms['displayByName'];
-                const displayByEmail = document.forms['displayByEmail'];
-                const displayByCommentId = document.forms['displayByCommentId'];
-
-                document.querySelectorAll('[id=editUserBtn]')[0].addEventListener("click", byUsernameEnableField);
-                document.querySelectorAll('[id=editUserBtn]')[1].addEventListener("click", byEmailEnableField);
-                document.querySelectorAll('[id=editUserBtn]')[2].addEventListener("click", byCommentIdEnableField);
-
-                for(var i = 0; i < displayByName.length; i++){
-                    displayByName[i].disabled = true;
-                    displayByEmail[i].disabled = true;
-                    displayByCommentId[i].disabled = true;
-                }
-                
-                function byUsernameEnableField(){               
-                    for(var i = 0; i < displayByName.length; i++){
-                        displayByName[i].disabled = false;
-                    }
-                    document.querySelectorAll('[id=enableUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=disableUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=deleteUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=saveUserBtn]')[0].setAttribute("style", "background-color: #2fc363;");
-                }
-                function byEmailEnableField(){                 
-                    for(var i = 0; i < displayByEmail.length; i++){
-                        displayByEmail[i].disabled = false;
-                    }   
-                    document.querySelectorAll('[id=enableUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=disableUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=deleteUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=saveUserBtn]')[1].setAttribute("style", "background-color: #2fc363;");         
-                }
-                function byCommentIdEnableField(){                 
-                    for(var i = 0; i < displayByCommentId.length; i++){
-                        displayByCommentId[i].disabled = false;
-                    }                    
-                    document.querySelectorAll('[id=enableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=disableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=deleteUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
-                    document.querySelectorAll('[id=saveUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
-                }
-
-                document.getElementById("<?php echo $_SESSION['defaultTabID']?>").click();
-            </script>
+            <!-- <script>
+            </script> -->
         </main>    
     </body>
 </html> 
