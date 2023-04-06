48732544be877593a439f945d2e00d8c69dc7c3c
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 14:54:56 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -1,7 +1,7 @@
 var loginId = document.getElementById("loginId"); //Login Id (Username or Email)
 var password = document.getElementById("password"); // Login Password
 var loginBtn = document.getElementById("loginSubmit"); // Login button
-var loginForm = document.forms['LoginForm']; // Login Form
+const loginForm = document.forms['LoginForm']; // Login Form
 
 // const emailDomains = ["gmail","yahoo","outlook"]
 const passwordRegex = "^[a-zA-Z]\w{8,16}$ "; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
