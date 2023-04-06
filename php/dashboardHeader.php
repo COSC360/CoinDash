3330c27939bea7306aac719f12f2c20190758a3d
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 21:24:52 -0700

    Added title logo(new)

--- php/dashboardHeader.php
+++ php/dashboardHeader.php
@@ -11,35 +11,34 @@
         </div>
         <div class="middle">
             <nav>
-                <div class="relative-container">
-                    <?php
-                        if(isset($_SESSION['id']) && $_SESSION['userType'] == "admin"){
-                            echo
-                            "
-                            <div class = \"adminTopNav\">
-                                <a href=\"https://cosc360.ok.ubc.ca/suyash06/project-JasonR24/php/adminAnalytics.php\" id=\"stats\">
-                                    Analytics
-                                </a>
-                                <a href=\"https://cosc360.ok.ubc.ca/suyash06/project-JasonR24/php/admin.php\">
-                                    Dashboard
-                                </a>
-                            </div>";
-                        }else{
-                            echo
-                            "
-                            <div class=\"relative-container\">
-                                <a href=\"#\" id=\"search\">
-                                    Search
-                                </a>
-                                <div style=\"position: absolute; left: 0; bottom:-2em;\">
-                                    <form id=\"search-modal\" class=\"hide\">
-                                        <input type=\"text\" name=\"like\" placeholder=\"What are you looking for?\">
-                                    </form>
-                                </div>
+                <?php
+                    if(isset($_SESSION['id']) && $_SESSION['userType'] == "admin"){
+                        echo
+                        "
+                        <div class = \"topNav\">
+                            <a href=\"https://cosc360.ok.ubc.ca/suyash06/project-JasonR24/php/adminAnalytics.php\" id=\"stats\">
+                                Analytics
+                            </a>
+                            <a href=\"https://cosc360.ok.ubc.ca/suyash06/project-JasonR24/php/admin.php\">
+                                Dashboard
+                            </a>
+                        </div>";
+                    }else{
+                        echo
+                        "
+                        <div class=\"relative-container\">
+                            <a href=\"#\" id=\"search\">
+                                Search
+                            </a>
+                            <div style=\"position: absolute; left: 0; bottom:-2em;\">
+                                <form id=\"search-modal\" class=\"hide\">
+                                    <input type=\"text\" name=\"like\" placeholder=\"What are you looking for?\">
+                                </form>
                             </div>
-                            <a href=\"https://cosc360.ok.ubc.ca/suyash06/project-JasonR24/php/dashboard.php\">My Dashboard</a>";
-                        }
-                    ?>
+                        </div>
+                        <a href=\"https://cosc360.ok.ubc.ca/suyash06/project-JasonR24/php/dashboard.php\">My Dashboard</a>";
+                    }
+                ?>
             </nav>
         </div>
         <div class="right settings">
