c060bea3b7bf896d3ac78e5ffb6ba2d179fdf4a7
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 13:30:51 -0700

    Editting Header

--- php/testURI.php
+++ php/testURI.php
@@ -1,12 +1,5 @@
 <?php
-    // $uri = $_SERVER['REQUEST_URI'];
-    // echo $uri."<br>"; // Outputs: URI
-
-    // $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
- 
-    // $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    // echo $url."<br>"; // Outputs: Full URL
-
-    $query = $_SERVER['QUERY_STRING'];
-    echo $query."<br>"; // Outputs: Query String
+    $curPageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);  
+    echo "The current page name is: ".$curPageName;  
+    echo "</br>";  
 ?>
