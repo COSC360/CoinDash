122c3896e5e6a57999cb23b8d7657f1a05eada28
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-05 17:56:58 -0700

    Building Admin Page v0.4

--- js/adminFormHandler.js
+++ js/adminFormHandler.js
@@ -1,13 +1,12 @@
 const displayByName = document.forms['displayByName'];
 const displayByEmail = document.forms['displayByEmail'];
 const displayByCommentId = document.forms['displayByCommentId'];
-const c = [document.querySelectorAll('[id=enableUserBtn]'), document.querySelectorAll('[id=disableUserBtn]'),document.querySelectorAll('[id=deleteUserBtn]'),document.querySelectorAll('[id=saveUserBtn]')];
 
 document.querySelectorAll('[id=editUserBtn]')[0].addEventListener("click", byUsernameEnableField);
 document.querySelectorAll('[id=editUserBtn]')[1].addEventListener("click", byEmailEnableField);
 document.querySelectorAll('[id=editUserBtn]')[2].addEventListener("click", byCommentIdEnableField);
 
-function addEnable(i){
+function enableBtn(i){
     document.querySelectorAll('[id=enableUserBtn]')[i].setAttribute("style", "background-color: #2fc363;");
     document.querySelectorAll('[id=disableUserBtn]')[i].setAttribute("style", "background-color: #2fc363;");
     document.querySelectorAll('[id=deleteUserBtn]')[i].setAttribute("style", "background-color: #2fc363;");
@@ -24,21 +23,17 @@ function byUsernameEnableField(){
     for(var i = 0; i < displayByName.length; i++){
         displayByName[i].disabled = false;
     }
-    addEnable(0);
+    enableBtn(0);
 }
 function byEmailEnableField(){                 
     for(var i = 0; i < displayByEmail.length; i++){
         displayByEmail[i].disabled = false;
     }
-    addEnable(1);            
+    enableBtn(1);            
 }
 function byCommentIdEnableField(){                 
     for(var i = 0; i < displayByCommentId.length; i++){
         displayByCommentId[i].disabled = false;
     }
-    addEnable(2);                    
-    // document.querySelectorAll('[id=enableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
-    // document.querySelectorAll('[id=disableUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
-    // document.querySelectorAll('[id=deleteUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
-    // document.querySelectorAll('[id=saveUserBtn]')[2].setAttribute("style", "background-color: #2fc363;");
+    enableBtn(2);                    
 }
