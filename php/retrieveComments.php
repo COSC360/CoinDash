a754b8ba582bcbd8136cec16f07974bdc166d01d
Jason Ramos <ramosjasonm.work@gmail.com>
2023-04-07 10:52:08 -0700

    Initial Nested Comments

--- php/retrieveComments.php
+++ php/retrieveComments.php
@@ -19,7 +19,7 @@
 
     function getCommentHTML($con, $commentData, $level){
         $commentHTML = "
-            <div style=\"margin-left:".($level)."em;\" class=\"comment-container\"> 
+            <div style=\"margin-left:".($level * 2)."em;\" class=\"comment-container\"> 
                 <hr class=\"medium-spacing\">
                 <div class=\"comment\" id=\"comment-1\">
                     <div class=\"main-comment\">
