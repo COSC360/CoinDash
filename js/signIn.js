c3c8f5ced36e6fafaead789e1b1ffb8b5a0bc327
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 21:09:00 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -3,10 +3,11 @@ const requiredInput = document.querySelectorAll(".required");
 var errorTextElement = document.querySelectorAll(".errorText");
 
 // <i class="fa-sharp fa-solid fa-circle-check" style="color: #11e43b;"></i>
-// <i class="fa-sharp fa-solid fa-circle-xmark" style="color: #ff0000;"></i>
+// 
 
-const passwordRegex = "^[a-zA-Z]\w{8,16}$ "; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
-const emailRegex = "^(.+)@([^\.].*)\.([a-z]{2,})$"; //Email validation based on current standard naming rules
+const passwordRegex = /^[a-zA-Z]\w{8,16}$/; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
+const emailRegex = /^(.+)@([^\.].*)\.([a-z]{2,})$/; //Email validation based on current standard naming rules
+const emailDomains = ["gmail","outlook","yahoo"];
 
 function showError(i,errMsg){
     errorTextElement[i].innerText = errMsg;
@@ -38,6 +39,14 @@ loginForm.addEventListener("submit",function(e){
     {
         error = true;
         showError(errorTextElement[1], "Password cannot be empty");
+    }else if(!passwordRegex.test(password))
+    {
+        error = true;
+        showError(errorTextElement[1], "Password is invalid. It must be between 8-16 characters");
+    }else if(emailDomains.includes(loginId) && !emailRegex.test(loginId))
+    {
+        error = true;
+        showError(errorTextElement[1], "Email is invalid");
     }
 
     if (error == true){
--- php/signIn.php
+++ php/signIn.php
@@ -47,12 +47,14 @@
                     <div class="login-box">
                         <form name = "loginForm" id ="loginForm" action= "processLogin.php" method="GET" required>
                             <div class="item-1">
-                                <label>Username or Email</label><br>     
+                                <label>Username or Email</label><br>
+                                <i class="fa-sharp fa-solid fa-circle-xmark" style="color: #ff0000;"></i>     
                                 <input type = "text" name = "loginId" id= "loginId" placeholder="What’s Your Registered Username or Email?" class="required" />
                                 <p class="errorText"></p>
                             </div>
                             <div class="item-2">
                                 <label>Password</label><br>
+                                <i class="fa-sharp fa-solid fa-circle-xmark" style="color: #ff0000;"></i>
                                 <input type = "password" name = "password" id= "password" placeholder="What’s Your Password?" class="required" />
                                 <p class="errorText"></p>
                             </div>
