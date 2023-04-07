47a668a348f7f1587597df4931db4279ed6606af
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 19:47:25 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -7,7 +7,6 @@ var loginBtn = document.getElementById("loginSubmit"); // Login button
 const passwordRegex = "^[a-zA-Z]\w{8,16}$ "; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
 const emailRegex = "^(.+)@([^\.].*)\.([a-z]{2,})$"; //Email validation based on current standard naming rules
 
-
 function showError(inputField,errMsg){
     inputField.innerText = errMsg;
 }
@@ -28,16 +27,28 @@ loginForm.addEventListener("submit",function(e){
 
     if(loginId == '' && password == '')
     {
-        err = true;
+        err |= true;
         showError(errorText[0], "Login ID cannot be empty");
         showError(errorText[1], "Password cannot be empty");
     }
     else
+
+    
+    if(loginId == '')
     {
-        // onkeydown clear error
+        err |= true;
+        showError(errorText[0], "Login ID cannot be empty");
     }
 
-    if (err = true)
+
+    if(password == '')
+    {
+        err |= true;
+        showError(errorText[1], "Password cannot be empty");
+    }
+
+
+    if (err == true)
     {
         e.preventDefault();
     }
