b869e82f7df64b52228977092e30d44d23f5abb7
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 20:59:31 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -8,9 +8,9 @@ var errorTextElement = document.querySelectorAll(".errorText");
 const passwordRegex = "^[a-zA-Z]\w{8,16}$ "; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
 const emailRegex = "^(.+)@([^\.].*)\.([a-z]{2,})$"; //Email validation based on current standard naming rules
 
-function showError(errorText,errMsg){
-    errorText.innerText = errMsg;
-    errorText.classList.add("showError");
+function showError(i,errMsg){
+    errorTextElement[i].innerText = errMsg;
+    errorTextElement[i].classList.add("showError");
 }
 
 function hideError(inputField){
@@ -28,8 +28,8 @@ loginForm.addEventListener("submit",function(e){
     if(loginId == '' && password == '')
     {
         error = true;
-        showError(errorTextElement[0], "Login ID cannot be empty");
-        showError(errorTextElement[1], "Password cannot be empty");
+        showError(0, "Login ID cannot be empty");
+        showError(1, "Password cannot be empty");
     }else if(loginId == '')
     {
         error = true;
