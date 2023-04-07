8fe789ad9e5a9efdab3c15eaeefa5d651b1a6fd1
Jason Ramos <ramosjasonm.work@gmail.com>
2023-04-07 09:00:28 -0700

    Optimizing dashboard for Guest

--- php/uploadDashboard.php
+++ php/uploadDashboard.php
@@ -3,9 +3,10 @@ session_start();
 include "DBconnection.php";
 include "sql-queries.php";
 
+echo "<script>console.log('Check')</script>";
 $userId = $_SESSION["Id"];
 $dashboardJSON = $_POST["dashboardJSON"];
-
+echo "<script>console.log('Check')</script>";
 uploadDashboard($con, $userId, $dashboardJSON);
     
 ?>
