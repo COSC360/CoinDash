660ed44c59114c9e7fee60f0aa5336a73735a9d3
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-07 13:32:42 -0700

    custom alertbox

--- php/signIn.php
+++ php/signIn.php
@@ -3,7 +3,6 @@
     if(isset($_SESSION['statusMsg'])){
         // echo "<script>window.alert(\"".$_SESSION['statusMsg']."\")</script>";
         include 'alert.php';
-        session_destroy();
     }else{
         session_destroy();
     }
