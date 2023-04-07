c273d4b0de8a915036e2f194aa3a7c64e448e80b
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-07 01:44:40 -0700

    Added env varibale

--- php/DBconnection.php
+++ php/DBconnection.php
@@ -1,20 +1,20 @@
 <?php
 // Database configuration  
-// $dbHost     = "cosc360.ok.ubc.ca";  
-// $dbUsername = "83864363";  
-// $dbPassword = "83864363";  
-// $dbName     = "db_83864363";
-require_once realpath(__DIR__."/vendor/autoload.php"); 
+$dbHost     = "cosc360.ok.ubc.ca";  
+$dbUsername = "83864363";  
+$dbPassword = "83864363";  
+$dbName     = "db_83864363";
+// require_once realpath(__DIR__."/vendor/autoload.php"); 
 
-use Dotenv\Dotenv;
+// use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
-$dbHost     = getenv("DB_HOST");  
-$dbUsername = getenv("DB_USERNAME");  
-$dbPassword = getenv("DB_PASSWORD"); 
-$dbName     = getenv("DB_NAME");
+// $dbHost     = getenv("DB_HOST");  
+// $dbUsername = getenv("DB_USERNAME");  
+// $dbPassword = getenv("DB_PASSWORD"); 
+// $dbName     = getenv("DB_NAME");
 
 echo $dbHost;
 // Create database connection  
