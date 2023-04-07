9cd7ac3560fddd78f348a580e2a823eeccc92d4b
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-07 13:22:30 -0700

    Adding comment history

--- js/signIn.js
+++ js/signIn.js
@@ -1,3 +1,14 @@
+const loginForm = document.forms['loginForm']; // Login Form
+const requiredInput = document.querySelectorAll(".required");
+let errorTextElement = document.querySelectorAll(".errorTextLogin");
+let errorLogoElement = document.querySelectorAll(".errorLogoLogin");
+
+// <i class="fa-sharp fa-solid fa-circle-check" style="color: #11e43b;"></i>
+
+const passwordRegex = /^[a-zA-Z]\w{8,16}$/; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
+const emailRegex = /^(.+)@([^\.].*)\.([a-z]{2,})$/; //Email validation based on current standard naming rules
+const emailDomains = ["gmail","outlook","yahoo"];
+
 function showError(i,errMsg){
     errorTextElement[i].innerText = errMsg;
     errorTextElement[i].classList.add("showError");
@@ -10,17 +21,6 @@ function hideError(i){
         errorTextElement[i].innerText = "";
     }
 }
-
-const loginForm = document.forms['loginForm']; // Login Form
-const requiredInput = document.querySelectorAll(".required");
-let errorTextElement = document.querySelectorAll(".errorTextLogin");
-let errorLogoElement = document.querySelectorAll(".errorLogoLogin");
-
-// <i class="fa-sharp fa-solid fa-circle-check" style="color: #11e43b;"></i>
-
-const passwordRegex = /^[a-zA-Z]\w{8,16}$/; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
-const emailRegex = /^(.+)@([^\.].*)\.([a-z]{2,})$/; //Email validation based on current standard naming rules
-const emailDomains = ["gmail","outlook","yahoo"];
 
 loginForm.addEventListener("submit",function(e){
     const loginId = requiredInput[0].value;
@@ -62,6 +62,5 @@ loginForm.addEventListener("submit",function(e){
 
 });
 
-loginForm.addEventListener("reset", console.log("clicked reset !"));
  
 
