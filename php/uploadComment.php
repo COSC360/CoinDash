de5ffc52b819fbf152a01cde188851ec5afe1e78
Jason Ramos <ramosjasonm.work@gmail.com>
2023-04-07 15:48:25 -0700

    Initial reply to comments

--- php/uploadComment.php
+++ php/uploadComment.php
@@ -3,16 +3,11 @@
     include "DBconnection.php";
     include "sql-queries.php";
 
-    // $coinId = $_POST["coinId"];
-    // $text = $_POST["text"];
-    // $parentId = isset($_POST["parentId"]) ? $_POST["parentId"] : null;
+    $coinId = $_POST["coinId"];
+    $text = $_POST["text"];
+    $parentId = isset($_POST["parentId"]) ? $_POST["parentId"] : null;
 
-    // $userId = $_SESSION["id"];
+    $userId = $_SESSION["id"];
 
-    $coinId = "axie-infinity";
-    $text = "NEW COOOMENT";
-    $parentId = 27;
-
-    $userId = 5;
     uploadComment($con, $userId, $coinId, $text, $parentId);
 ?>
