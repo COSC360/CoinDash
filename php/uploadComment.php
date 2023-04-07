f64a5498060d4fd1f9299055b3619133787ce256
Jason Ramos <ramosjasonm.work@gmail.com>
2023-04-07 15:45:17 -0700

    Initial reply to comments

--- php/uploadComment.php
+++ php/uploadComment.php
@@ -1,15 +1,14 @@
 <?php
-    session_set_cookie_params(0);
     session_start();
     include "DBconnection.php";
     include "sql-queries.php";
 
     $coinId = $_POST["coinId"];
     $text = $_POST["text"];
+    echo $text;
     $parentId = isset($_POST["parentId"]) ? $_POST["parentId"] : null;
 
     $userId = $_SESSION["id"];
     echo $parentId;
-    echo $text;
     uploadComment($con, $userId, $coinId, $text, $parentId);
 ?>
