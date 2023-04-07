7c35539ded3c90ea3b0beb08b16e6d3085b47e66
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 18:39:27 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -1,7 +1,8 @@
-var loginId = document.getElementById("loginId"); //Login Id (Username or Email)
-var password = document.getElementById("password"); // Login Password
-var loginBtn = document.getElementById("loginSubmit"); // Login button
 var loginForm = document.forms['loginForm']; // Login Form
+var loginId = loginForm[0].value; //Login Id (Username or Email)
+var password = loginForm[1].value; // Login Password
+var loginBtn = document.getElementById("loginSubmit"); // Login button
+
 
 
 // <i class="fa-sharp fa-solid fa-circle-check" style="color: #11e43b;"></i>
@@ -10,31 +11,36 @@ var loginForm = document.forms['loginForm']; // Login Form
 const passwordRegex = "^[a-zA-Z]\w{8,16}$ "; //Simple password expression. The password must be at least 8 characters but no more than 16 characters long
 const emailRegex = "^(.+)@([^\.].*)\.([a-z]{2,})$"; //Email validation based on current standard naming rules
 var errMsg = "";
-var errState = true;
 
-loginForm.addEventListener("submit",function(e){
-    for(var i = 0; i < 2; i++){
-        if(loginForm[i].value != null){
-            errState = false;
-        }else{
-            errState = true;
-        }
-    }
-    if(errState = true){
-        console.log("Fields cannot be empty !");
-        e.preventDefault();
-    }
-});
+console.log(loginForm);
+// loginForm.addEventListener("submit",function(e){
+//     if(loginId == null && password == null){
+//         displayError(false);
+//     }
+//     if(errState = true){
+//         displayError(true);
+//         e.preventDefault();
+//     }
+// });
  
+// function displayError(errState){
+//     if(errState == true){
+//         showError();
+//     }
 
-function showError(){
+//     if(errState == false){
+//         hideError();
+//     }   
+// }
 
-}
+// function showError(){
 
-function hideError(){
+// }
 
-}
+// function hideError(){
 
-function validEntry(){
+// }
 
-}
+// function validEntry(){
+
+// }
