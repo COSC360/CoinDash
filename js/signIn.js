94eca0ceaad4c3f60929e8bac403917fa750b407
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 21:30:31 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -21,43 +21,46 @@ function hideError(inputField){
 }
 
 loginForm.addEventListener("submit",function(e){
+    e.preventDefault();
+
     let loginId = requiredInput[0].value;
     let password = requiredInput[1].value;
     console.log(requiredInput[0].value);
     console.log(requiredInput[1].value);
+
     var error = false;
 
 
-    if(loginId == '' && password == '')
-    {
-        error = true;
-        showError(0, "Login ID cannot be empty");
-        showError(1, "Password cannot be empty");
-    }else if(loginId == '')
-    {
-        error = true;
-        showError(errorTextElement[0], "Login ID cannot be empty");
-    }else if(password == '')
-    {
-        error = true;
-        showError(errorTextElement[1], "Password cannot be empty");
-    }else if(!passwordRegex.test(password))
-    {
-        error = true;
-        showError(errorTextElement[1], "Password is invalid. It must be between 8-16 characters");
-    }else if(emailDomains.includes(loginId) && !emailRegex.test(loginId))
-    {
-        error = true;
-        showError(errorTextElement[1], "Email is invalid");
-    }
+    // if(loginId == '' && password == '')
+    // {
+    //     error = true;
+    //     showError(0, "Login ID cannot be empty");
+    //     showError(1, "Password cannot be empty");
+    // }else if(loginId == '')
+    // {
+    //     error = true;
+    //     showError(errorTextElement[0], "Login ID cannot be empty");
+    // }else if(password == '')
+    // {
+    //     error = true;
+    //     showError(errorTextElement[1], "Password cannot be empty");
+    // }else if(!passwordRegex.test(password))
+    // {
+    //     error = true;
+    //     showError(errorTextElement[1], "Password is invalid. It must be between 8-16 characters");
+    // }else if(emailDomains.includes(loginId) && !emailRegex.test(loginId))
+    // {
+    //     error = true;
+    //     showError(errorTextElement[1], "Email is invalid");
+    // }
 
-    console.log(error);
+    // console.log(error);
 
-    if (error = true){
-        e.preventDefault();
-    }else{
-        e.preventDefault();
-    }
+    // if (error = true){
+    //     e.preventDefault();
+    // }else{
+    //     e.preventDefault();
+    // }
 
 });
  
