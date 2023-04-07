0df839f2c2713e5e7abe411ea57ba2feff12fe89
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-06 21:27:42 -0700

    Client Side validation v0.1

--- js/signIn.js
+++ js/signIn.js
@@ -23,8 +23,8 @@ function hideError(inputField){
 loginForm.addEventListener("submit",function(e){
     let loginId = requiredInput[0].value;
     let password = requiredInput[1].value;
-    console.log(loginId);
-    console.log(password);
+    console.log(requiredInput[0].value);
+    console.log(requiredInput[0].value);
     var error = false;
 
 
