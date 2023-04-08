b09b96e1aae16bda261284663dddd4f55e4a70b3
Jason Ramos <ramosjasonm.work@gmail.com>
2023-04-07 18:11:32 -0700

    Contextual Number Formatting

--- php/fullCoin.php
+++ php/fullCoin.php
@@ -1,4 +1,6 @@
 <?php
+    include "utils.php";
+
     $coinId = $_GET["coinId"];
     $coin = retrieveCoinById($con, $coinId);
     $coin = $coin[0];
@@ -11,11 +13,11 @@
         <div class=\"product-info-container\">
             <h3>".$coin["name"]."</h3>
             <div class=\"current-price-container\">
-                <strong class=\"product-price\">".number_format($coin["usd"], 4, '.', '')." USD</strong>
-                <strong class=\"product-price\">".number_format($coin["cad"], 4, '.', '')." CAD</strong>
-                <strong class=\"product-price\">".number_format($coin["eur"], 4, '.', '')." EUR</strong>
-                <strong class=\"product-price\">".number_format($coin["php"], 4, '.', '')." PHP</strong>
-                <strong class=\"product-price\">".number_format($coin["jpy"], 4, '.', '')." JPY</strong>
+                <strong class=\"product-price ".getPriceColorClass($coin["price_change_24h"])."\">".number_format($coin["usd"], 4, '.', '')." USD</strong>
+                <strong class=\"product-price ".getPriceColorClass($coin["price_change_24h"])."\">".number_format($coin["cad"], 4, '.', '')." CAD</strong>
+                <strong class=\"product-price ".getPriceColorClass($coin["price_change_24h"])."\">".number_format($coin["eur"], 4, '.', '')." EUR</strong>
+                <strong class=\"product-price ".getPriceColorClass($coin["price_change_24h"])."\">".number_format($coin["php"], 4, '.', '')." PHP</strong>
+                <strong class=\"product-price ".getPriceColorClass($coin["price_change_24h"])."\">".number_format($coin["jpy"], 4, '.', '')." JPY</strong>
             </div>
             <div class=\"price-trend-container\">
                 <p>7D: ".number_format($coin["price_change_7d"], 2, '.', '')."%</p>
