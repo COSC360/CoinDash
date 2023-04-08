12df6f1af644bd5eff83f0149204ea7620eb5f4e
SleepyPotato <suyash.aminbhavi@gmail.com>
2023-04-07 20:32:34 -0700

    fixing account.php css

--- php/dashboardHeader.php
+++ php/dashboardHeader.php
@@ -66,7 +66,7 @@
                     if (isset($_SESSION["id"])){
                         echo 
                         "<div class=\"container fit-width\">
-                        <p>Hi&nbsp;&nbsp;".$_SESSION["username"]." !&nbsp;&nbsp;|&nbsp;&nbsp;</p>
+                        <p>Hi&nbsp;&nbsp;".$_SESSION["username"]."&nbsp;&nbsp;|&nbsp;&nbsp;</p>
                         <a href=\"../php/logout.php\">Logout</a>
                         </div>";                
                     }else{
