95c165371830d487504fabc7cb767318e9a06350
Jason Ramos <ramosjasonm.work@gmail.com>
2023-04-07 19:40:21 -0700

    Contextual Number Formatting

--- php/retrieveComments.php
+++ php/retrieveComments.php
@@ -3,13 +3,14 @@
     include "sql-queries.php";
 
     $coinId = $_POST["coinId"];
+    $userType = isset($_SESSION["userType"]) ?? 'regular';
 
     $comments = retrievePostComment($con, $coinId);
     
     $output = "";
 
     foreach($comments as $comment) {
-        $output .= getCommentHTML($con, $comment, 0);
+        $output .= getCommentHTML($con, $comment, 0, $userType);
     }
 
     if ($output === ""){
@@ -18,7 +19,7 @@
         echo $output;
     }
 
-    function getCommentHTML($con, $commentData, $level){
+    function getCommentHTML($con, $commentData, $level, $userType){
         $commentHTML = "
             <div style=\"margin-left:".($level * 2)."em;\" class=\"comment-container\"> 
                 <hr class=\"medium-spacing\">
@@ -31,9 +32,13 @@
                         </div>
                         <p class=\"review-content\">".$commentData["text"]."</p>
                     </div>
-                    <p class=\"reply-btn\">Reply</p> | 
-                    <p class=\"delete-btn\">Delete</p>
-                    <form class=\"reply-form\">
+                    <p class=\"reply-btn\">Reply</p> | ";
+                    
+                    if ($userType == "admin"){
+                        $commentHTML .= "<p class=\"delete-btn\">Delete</p>";
+                    }
+
+        $commentHTML .="<form class=\"reply-form\">
                         <textarea name=\"text\"></textarea>
                         <p class=\"btn-container\">
                             <button type=\"submit\" class=\"comment-btn\">Reply</button>
@@ -46,7 +51,7 @@
         $replies = retrieveCommentReplies($con, $commentData["id"]);
         if ($replies){
             foreach($replies as $reply) {
-                $commentHTML .= getCommentHTML($con, $reply, $level + 1);
+                $commentHTML .= getCommentHTML($con, $reply, $level + 1, $userType);
             }
         }
 
